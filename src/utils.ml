dda0d939c0695bf910032fefc41ac2dff31ab7e1
Gareth Smith <gds@doc.ic.ac.uk>
2012-02-25 00:20:50 +0000

    Refactored the store predicate inference rules to use the new extract functions. Hopefully we'll be able to do frame inference and biabduction more easily with this style.

--- src/utils.ml
+++ src/utils.ml
@@ -2,3 +2,6 @@ open List
 
 let flat_map f l = flatten (map f l)
 
+(* This exception is raised when Gareth thinks something is impossible,
+   but it happens anyway. *)
+exception ThereAreMorePossibilitiesThanYouThink
