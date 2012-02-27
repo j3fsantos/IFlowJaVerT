c71ac241fcfc4d803cc26c2a783d81847ff6f354
Gareth Smith <gds@ic.ac.uk>
2012-02-27 18:34:30 +0000

    Added some more store-level inference rules, for if we're one call level down.

--- src/utils.ml
+++ src/utils.ml
@@ -5,3 +5,7 @@ let flat_map f l = flatten (map f l)
 (* This exception is raised when Gareth thinks something is impossible,
    but it happens anyway. *)
 exception ThereAreMorePossibilitiesThanYouThink
+
+(* For some reason, ocaml doesn't seem to have a boolean implication operator. Weird. *)
+
+let (==>) a b = (! a) || b
