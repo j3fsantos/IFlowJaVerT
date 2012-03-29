c2613bbb1b5a48b8416534b6aff2e37325966168
Daiva Naudziuniene <daiva.naudziuniene@gmail.com>
2012-03-29 17:09:20 +0100

    Getting postconditions.

--- src/parsing/parsing_utils.ml
+++ src/parsing/parsing_utils.ml
@@ -8,13 +8,16 @@ let parse_formula f =
   let lexbuf = Lexing.from_string f in
   Formula_parser.main Formula_lexer.token lexbuf
   
-let get_pre_from_code exp =
-  let rec get_req annots =
+let get_annot_from_code exp annot_type = 
+    let rec get_annot annots =
     match annots with
       | [] -> raise No_Spec_In_Code
       | annot :: tl -> 
-        if (is_requires_annot annot) then annot
-        else get_req tl in
-  let req_annot = get_req exp.exp_annot in
-  let f = parse_formula req_annot.aformula in
-  substitute_locs f
+        if (annot.atype = annot_type) then annot
+        else get_annot tl in
+  let annot = get_annot exp.exp_annot in
+  parse_formula annot.aformula
+  
+let get_pre_from_code exp = get_annot_from_code exp Requires
+  
+let get_post_from_code exp = get_annot_from_code exp Ensures
