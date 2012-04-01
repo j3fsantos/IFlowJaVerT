d3a966dfc205dcdcfbab116888771cf13ddb7649
Daiva Naudziuniene <daiva.naudziuniene@gmail.com>
2012-04-01 19:53:35 +0100

    Added two tests for the first example with postcondition. TODO : to remove printing if test does not fail.

--- tests/test_all.ml
+++ tests/test_all.ml
@@ -3,7 +3,8 @@ open OUnit
 let suite = "Test The Lot" >:::
   [ Logictests.suite;
     Parsing_tests.suite;
-    Utils_tests.suite
+    Utils_tests.suite;
+    Examples_tests.suite
   ]
 
 let _ = run_test_tt_main suite
