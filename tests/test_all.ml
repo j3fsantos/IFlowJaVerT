cd6db9081010d1514e352beb1a72bf1c19ebe0eb
Daiva Naudziuniene <daiva.naudziuniene@gmail.com>
2012-04-12 23:16:38 +0100

    Five more examples from FireFox test suite.

--- tests/test_all.ml
+++ tests/test_all.ml
@@ -1,11 +1,11 @@
 open OUnit
 
 let suite = "Test The Lot" >:::
-  [ Logictests.suite;
+  [ Paper_examples_tests.suite;
+    Logictests.suite;
     Parsing_tests.suite;
     Utils_tests.suite;
     Examples_tests.suite;
-    Paper_examples_tests.suite;
   ]
 
 let _ = run_test_tt_main suite
