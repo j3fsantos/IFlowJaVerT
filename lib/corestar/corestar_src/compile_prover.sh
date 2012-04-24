db2381f582dad5dba299372512fdff44eb99b5ca
Daiva Naudziuniene <daiva.naudziuniene@gmail.com>
2012-04-24 16:31:58 +0100

    coreStar source files.

---
+++ lib/corestar/corestar_src/compile_prover.sh
@@ -0,0 +1,8 @@
+ocamlbuild \
+   -I src/utils \
+   -I src/prover_syntax \
+   -I src/plugin_interface \
+   -I src/prover \
+   -I src/symbexe_syntax \
+   -I src/parsing \
+   corestar.cma
---
+++ lib/corestar/corestar_src/src/utils/config.mli
@@ -0,0 +1,28 @@
+(********************************************************
+   This file is part of coreStar
+        src/utils/config.mli
+   Release
+        $Release$
+   Version
+        $Rev$
+   $Copyright$
+
+   coreStar is distributed under a BSD license,  see,
+      LICENSE.txt
+ ********************************************************)
+
+
+val specs_template_mode : bool ref
+val dotty_print : bool ref
+val symb_debug : unit -> bool
+val eclipse_ref : bool ref
+val parse_debug : unit -> bool
+val smt_debug : unit -> bool
+val abs_int_join : unit -> bool
+val solver_path : string ref
+val smt_run : bool ref
+val smt_custom_commands : string ref
+val args_default : (string * Arg.spec * string) list
+val verb_proof : unit -> bool
+val eclipse_mode : unit -> bool
+val abs_int_plugins : string list ref
