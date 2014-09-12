134c66dc907987a351afba39c730cbf1cd0b778d
Daiva Naudziuniene <daiva.naudziuniene@gmail.com>
2014-09-12 11:50:03 +0100

    Reordering files.

---
+++ src/pulp/interpreter/interpreter_Print.ml
@@ -0,0 +1,24 @@
+open Interpreter
+open Pulp_Syntax
+open Pulp_Syntax_Print
+
+let string_of_loc l =
+  match l with
+    | BLoc bl -> string_of_builtin_loc bl
+    | Loc l -> "l"^ (string_of_int l)
+
+let string_of_heap_value hv =
+  match hv with
+    | HVLiteral lit -> string_of_literal lit
+    | HVObj l -> string_of_loc l
+
+let string_of_value v =
+  match v with
+    | VHValue hv -> string_of_heap_value hv
+    | VRef (l, x, rt) -> 
+      Printf.sprintf "(%s .%s %s)" 
+      (string_of_loc l)
+      (match rt with
+        | MemberReference -> "_o"
+        | VariableReference -> "_v")
+      x
