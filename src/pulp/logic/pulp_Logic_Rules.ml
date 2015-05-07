8ce18e9a8004014a7784ea09c27aec634fcbb8fa
Daiva Naudziuniene <daiva.naudziuniene@gmail.com>
2015-05-07 17:29:25 +0100

    Small axioms for assignment and new object creation.

--- src/pulp/logic/pulp_Logic_Rules.ml
+++ src/pulp/logic/pulp_Logic_Rules.ml
@@ -1,4 +1,5 @@
 open Logic
+open Logic_Utils
 open Pulp_Syntax
 exception NotImplemented of string
 
@@ -34,3 +35,32 @@ let rec expr_to_logical_expr expr : Logic.logical_exp =
     | Field e -> Le_Field (f e)
     | IsTypeOf (e, t) -> raise (NotImplemented "is type of deprecated")
     | TypeOf e -> Le_TypeOf (f e)
+
+let small_axiom_basic_stmt bs : formula * formula =
+  match bs with
+    | Skip -> empty_f, empty_f
+    | Assignment a ->
+      let var = Le_Var (PVar a.assign_left) in
+      begin match a.assign_right with
+        | Expression e -> 
+          let old = fresh_e () in
+          let logic_e = expr_to_logical_expr e in
+          let vmap = VarMap.add (PVar a.assign_left) old VarMap.empty in
+          Logic.Eq (var, Le_Var old), Logic.Eq (var, subs_vars_in_exp vmap logic_e)
+        | Call _ 
+        | Eval _
+        | BuiltinCall _ -> raise (Invalid_argument "Call")
+        | Obj ->
+          let l = fresh_loc_e () in 
+          empty_f, 
+          Star [
+            Eq (var, Le_Value (Lv_Loc (Lb_Loc l)));
+            ObjFootprint (l, [])
+          ]
+        | HasField (e1, e2) -> raise (NotImplemented "HasField")
+        | Lookup (e1, e2) -> raise (NotImplemented "Lookup")
+        | Deallocation (e1, e2) -> raise (NotImplemented "Deallocation")
+        | ProtoF (e1, e2) -> raise (NotImplemented "ProtoField")
+        | ProtoO (e1, e2) -> raise (NotImplemented "ProtoObj")
+      end
+    | Mutation m -> raise (NotImplemented "Mutation")
