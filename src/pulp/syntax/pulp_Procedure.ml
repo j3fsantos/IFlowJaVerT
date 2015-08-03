929dbe1c1b64643eb6ac56f9f830fb1811be9c45
José Santos <josesantos@dyn1236-145.wlan.ic.ac.uk>
2015-08-03 15:57:44 +0100

    fixed bug

--- src/pulp/syntax/pulp_Procedure.ml
+++ src/pulp/syntax/pulp_Procedure.ml
@@ -21,7 +21,7 @@ let make_ctx_vars fid vars =
     label_throw : label;
     label_continue : (string * label) list;
     label_break : (string * label) list; 
-		breaks_to_switches : (Pulp_Syntax.variable, Pulp_Syntax.variable) Hashtbl.t
+		breaks_to_vars : (Pulp_Syntax.variable, Pulp_Syntax.variable) Hashtbl.t
   }
   
 let create_ctx env =
@@ -33,7 +33,7 @@ let create_ctx env =
      label_throw = "throw." ^ fresh_r ();
      label_continue = [];
      label_break = []; 
-		 breaks_to_switches = Hashtbl.create 123456
+		 breaks_to_vars = Hashtbl.create 123456
   }
   
 type function_block = { 
