cfa108ed327b900a542dc423f74a0738fb3caa1d
Daiva Naudziuniene <daiva.naudziuniene@gmail.com>
2015-10-21 19:10:14 +0100

    Gamma spec.

--- src/pulp/logic/spec_Fun_Specs.ml
+++ src/pulp/logic/spec_Fun_Specs.ml
@@ -13,7 +13,6 @@ let get_value_spec param ctx =
   let pre_not_a_ref = Star [not_type_of_mref_f param; not_type_of_vref_f param] in
   
   let v = Le_Var (fresh_a()) in
-  
   let pre_vref_obj = Star [
     type_of_vref_f param; 
     type_of_obj_f (Le_Base (Le_PVar param));
@@ -22,16 +21,29 @@ let get_value_spec param ctx =
     NEq (v, Le_None) 
   ] in 
   
-  (*let l = Le_Var (fresh_e()) in
-  let post_vref_obj = combine pre_vref_obj
+ let ls = Le_Var (fresh_e()) in
+ let l = Le_Var (fresh_e()) in 
+ let pre_vref_lg_empty = Star [
+    type_of_vref_f param; 
+    Eq (Le_Base (Le_PVar param), Le_Literal (LLoc Lg));
+    ProtoChain (Le_Literal (LLoc Lg), ls, l); 
+    Pi (mk_pi_pred ls (Le_Literal (LLoc Lg)) (Le_Field (Le_PVar param)) l v);
+    Eq (v, Le_Literal Empty) 
+  ] in
+  
+  
+  let lerror = Le_Var (fresh_e()) in
+  let post_vref_lg_empty = combine pre_vref_lg_empty
     (Star [
-      REq l;
-      proto_heaplet_f l (Le_Literal (LLoc Lrep));
-      class_heaplet_f l "Error"
-    ]) in*)
+      REq lerror;
+      proto_heaplet_f lerror (Le_Literal (LLoc Lrep));
+      class_heaplet_f lerror "Error"
+    ]) in
   
   [(mk_spec_with_excep pre_not_a_ref [combine pre_not_a_ref (REq (Le_PVar param))] [false_f]);
-   (mk_spec_with_excep pre_vref_obj [combine pre_vref_obj (REq v)] [false_f])] 
+   (mk_spec_with_excep pre_vref_obj [combine pre_vref_obj (REq v)] [false_f]);
+   (mk_spec_with_excep pre_vref_lg_empty [false_f] [post_vref_lg_empty])
+  ] 
   
 let get_value_fb () =
   let param = "x" in
