7f3172cd11c005d21ccb5af411286cfef3fb8ad3
Daiva Naudziuniene <daiva.naudziuniene@gmail.com>
2015-11-29 14:50:27 +0000

    An example.

--- tests/evaluation_Examples.ml
+++ tests/evaluation_Examples.ml
@@ -44,11 +44,52 @@ let test_8_7_2_1_s () =
       | _ -> fb
   ) p_exp in
   
-  
-  
   Printf.printf "Specs %s" (String.concat "\n" (List.map (fun (id, fb) -> Printf.sprintf "%s : %s" id (Pulp_Logic_Print.string_of_spec_pre_post_list fb.func_spec "\n")) (AllFunctions.bindings p_exp)));
   
   AllFunctions.iter (fun id f -> test_js_program_template "test_8_7_2_1_s" f p_exp) p_exp
   
+(* ch11/11.1/11.1.2/S11.1.2_A1_T2.js *)  
+let test_S11_1_2_A1_T2 () =
+  let js_program = "this.z; z;" in
+    
+  let p_exp, p_env = get_pexp js_program in
+  
+  let ls = Le_Var (fresh_e ()) in
+  let l = Le_Var (fresh_e ()) in
+  let pi = proto_pred_f ls (Le_Literal (LLoc Lop)) (Le_Literal (String "z")) l (Le_Literal Empty) in
+   
+  let lerror = Le_Var (fresh_e()) in
+  let post_ref_exception = combine empty_f
+    (Star [
+      REq lerror;
+      proto_heaplet_f lerror (Le_Literal (LLoc Lrep));
+      class_heaplet_f lerror "Error"
+    ]) in
+
+  let p_exp = AllFunctions.mapi (fun id fb ->
+    match id with
+      | "main" -> 
+        let undefined = Le_Var (fresh_e()) in
+        let proto = Le_Var (fresh_e()) in
+        let pre = Pulp_Logic_Utils.combine
+          pi 
+          (Star [
+          Eq (Le_PVar Pulp_Translate_Aux.rthis, Le_Literal (LLoc Lg)); 
+          Pulp_Logic.proto_heaplet_f (Le_Literal (LLoc Lg)) proto;
+          Heaplet (Le_Literal (LLoc Lg), Le_Literal (String "undefined"), undefined);
+          Heaplet (Le_Literal (LLoc Lg), Le_Literal (String "z"), Le_None);
+        ]) in
+        let spec_main = mk_spec_with_excep pre [false_f] [post_ref_exception] in
+        {fb with func_spec = [spec_main]}
+      | _ -> fb
+  ) p_exp in
+  
+  
+  
+  Printf.printf "Specs %s" (String.concat "\n" (List.map (fun (id, fb) -> Printf.sprintf "%s : %s" id (Pulp_Logic_Print.string_of_spec_pre_post_list fb.func_spec "\n")) (AllFunctions.bindings p_exp)));
+  
+  AllFunctions.iter (fun id f -> test_js_program_template "test_S11_1_2_A1_T2" f p_exp) p_exp
+  
  let suite = "Testing_Evaluation_Examples" >:::
-  [ "test_8_7_2_1_s" >:: test_8_7_2_1_s ]
+  [ "test_8_7_2_1_s" >:: test_8_7_2_1_s;
+    "test_S11_1_2_A1_T2" >:: test_S11_1_2_A1_T2 ]
