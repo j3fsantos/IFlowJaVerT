b3b979797de7d4563eddfd9ec36c4160fffb4bfe
jfaustin <josesantos@dyn1185-209.wlan.ic.ac.uk>
2016-05-26 19:45:57 +0100

    fixing ssa error

--- src/pulp/SJSIL/printing/SSyntax_Print.ml
+++ src/pulp/SJSIL/printing/SSyntax_Print.ml
@@ -137,7 +137,11 @@ let rec sexpr_of_bcmd bcmd i line_numbers_on =
 	| SPhiAssignment (var, var_arr) -> 
 		let var_arr_str = 
 			Array.fold_left 
-				(fun ac v -> ac ^ " " ^ v)
+				(fun ac v -> 
+					match v with 
+					| Some v -> ac ^ " " ^ v
+					| None -> ac ^ " $$empty "
+				)
 				""
 				var_arr in 
 		Printf.sprintf "'(%sv-phi-assign %s %s)" str_i var var_arr_str	
@@ -171,9 +175,13 @@ let rec string_of_bcmd bcmd i line_numbers_on escape_string =
 			if (i >= len) 
 				then str_ac 
 				else 
+					let var_arr_i_str = 
+						(match var_arr.(i) with 
+						| None -> "$$empty"
+						| Some v_i -> v_i) in 
 					(if (i == 0)
-						then loop 1 var_arr.(i)
-						else  loop (i + 1) (str_ac ^ ", " ^ var_arr.(i))) in 
+						then loop 1 var_arr_i_str
+						else  loop (i + 1) (str_ac ^ ", " ^ var_arr_i_str)) in 
 		let var_arr_str = loop 0 "" in 
 		Printf.sprintf "%s%s := PHI(%s)" str_i var var_arr_str						
 	(* x := new() *)
