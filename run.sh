ed090a0a944f3fa2887d6533dfb983f1dc6fc510
Thomas Wood <thomas.wood09@imperial.ac.uk>
2016-06-21 17:32:13 +0100

    Results bundle is huge, don't collect this information in autotest.

--- run.sh
+++ run.sh
@@ -5,5 +5,13 @@ verbose=${jsfile%.js}.verbose
 
 cd environment
 ./js2jsil_main.native -file $jsfile -harness
-./SJSIL_Parser_main.native -file $jsilfile -verbose -run &> $verbose || true
+
+if [ -z $CI ]; then
+  # Produce verbose version if not in auto-testing
+  ./SJSIL_Parser_main.native -file $jsilfile -verbose -run &> $verbose || true
+fi
 ./SJSIL_Parser_main.native -file $jsilfile -run
+
+if [ -n $CI ]; then
+  rm $jsilfile
+fi
