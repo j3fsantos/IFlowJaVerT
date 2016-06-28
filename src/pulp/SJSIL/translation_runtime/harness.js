620c46cee5281535180d3c6fa5f7697ab95db4b1
pmaksimo <p.maksimovic@imperial.ac.uk>
2016-06-28 11:07:55 +0100

    more little fixes

--- src/pulp/SJSIL/translation_runtime/harness.js
+++ src/pulp/SJSIL/translation_runtime/harness.js
@@ -256,3 +256,16 @@ function arrayContains(arr, expected) {
     }
     return true;
 }
+
+function compareArray(a, b) {
+	  if (b.length !== a.length) {
+	    return false;
+	  }
+
+	  for (var i = 0; i < a.length; i++) {
+	    if (b[i] !== a[i]) {
+	      return false;
+	    }
+	  }
+	  return true;
+	}
