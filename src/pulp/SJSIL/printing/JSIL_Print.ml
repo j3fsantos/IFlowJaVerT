c290689c9b8200e807a61e68a9d154040d0763ce
pmaksimo <p.maksimovic@imperial.ac.uk>
2016-08-18 18:06:01 +0200

    Error in JSIL_Print wrt floats

--- src/pulp/SJSIL/printing/JSIL_Print.ml
+++ src/pulp/SJSIL/printing/JSIL_Print.ml
@@ -20,11 +20,11 @@ let rec tabs_to_str i  =
 
 let string_of_float x = 
 	if (x == nan) 
-		then "+nan.0"
+		then "nan"
 		else if (x == neg_infinity) 
-			then "-inf.0"
+			then "-inf"
 			else if (x == infinity) 
-				then "+inf.0"
+				then "inf"
 				else string_of_float x 
 
 let string_of_binop bop = match bop with 
