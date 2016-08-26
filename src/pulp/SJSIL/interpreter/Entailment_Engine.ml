b9b6fdcded44ee4e0d267b35e703f537a6ed4b5e
Iván Matellanes <matellanesivan@gmail.com>
2016-08-26 15:59:33 +0100

    Proper encoding of strings and equality on Z3

--- src/pulp/SJSIL/interpreter/Entailment_Engine.ml
+++ src/pulp/SJSIL/interpreter/Entailment_Engine.ml
@@ -1,73 +1,93 @@
 open Z3
 open JSIL_Syntax
 
-(* Get a Z3 sort from a JSIL type *)
-let sort_of_type ctx t =
-	match t with
-	| UndefinedType         -> Sort.mk_uninterpreted_s ctx "UndefinedSort"
-	| NullType              -> Sort.mk_uninterpreted_s ctx "NullSort"
-	| EmptyType             -> Sort.mk_uninterpreted_s ctx "EmptySort"
-	| NoneType              -> Sort.mk_uninterpreted_s ctx "NoneSort"
-  | BooleanType           -> Boolean.mk_sort ctx
-	| IntType               -> Arithmetic.Integer.mk_sort ctx
-  | NumberType            -> Arithmetic.Real.mk_sort ctx
-	| StringType            -> Sort.mk_uninterpreted_s ctx "StringSort"
-  | ObjectType            -> Sort.mk_uninterpreted_s ctx "ObjectSort"
-	| ReferenceType         -> Sort.mk_uninterpreted_s ctx "ReferenceSort"
-	| ObjectReferenceType   -> Sort.mk_uninterpreted_s ctx "ObjectReferenceSort"
-	| VariableReferenceType -> Sort.mk_uninterpreted_s ctx "VariableReferenceSort"
-	| ListType              -> Sort.mk_uninterpreted_s ctx "ListSort"
-	| TypeType              -> Sort.mk_uninterpreted_s ctx "TypeSort"
+(** Retrieve the jsil_type corresponding to a bin_op *)
+let type_of_binary_operator op =
+	match op with
+	(* FIXME: These could be NumberType *)
+	| Plus  -> IntType
+	| Minus -> IntType
+	| Times -> IntType
+	| Div   -> IntType
+	| Mod   -> IntType
+	| _     -> raise (Failure "SMT encoding: Construct not supported yet!")
+
+(** Retrieve the jsil_type corresponding to a unary_op *)
+let type_of_unary_operator op =
+	match op with
+	(* FIXME: This could be NumberType *)
+	| UnaryMinus  -> IntType
+	| _           -> raise (Failure "SMT encoding: Construct not supported yet!")
+
+(** Retrieve the jsil_type corresponding to a jsil_logic_expr *)
+let type_of_logic_expr gamma expr =
+	match expr with
+	| LLit llit          -> JSIL_Interpreter.evaluate_type_of llit
+	| LNone              -> NoneType
+	| LVar var           ->
+		(try Hashtbl.find gamma var with
+		| _ -> raise (Failure "Type of logical variable not found in the environment: FIRE"))
+	| ALoc _             -> ObjectType
+	| PVar _             -> raise (Failure "Program variable in pure formula: FIRE")
+	| LBinOp (_, op, _)  -> type_of_binary_operator op
+	| LUnOp (op, _)      -> type_of_unary_operator op
+	| _                  -> raise (Failure "SMT encoding: Construct not supported yet!")
 
 
-(** Encode JSIL type literals as Z3 constants of sort TypeSort *)
+(** Encode JSIL type literals as Z3 numerical constants *)
 let encode_type ctx jsil_type =
-	let type_sort = sort_of_type ctx TypeType in
 	match jsil_type with
-	| UndefinedType         -> Expr.mk_const_s ctx "UndefinedType" type_sort
-	| NullType              -> Expr.mk_const_s ctx "NullType" type_sort
-	| EmptyType             -> Expr.mk_const_s ctx "EmptyType" type_sort
-	| NoneType              -> Expr.mk_const_s ctx "NoneType" type_sort
-  | BooleanType           -> Expr.mk_const_s ctx "BooleanType" type_sort
-	| IntType               -> Expr.mk_const_s ctx "IntType" type_sort
-  | NumberType            -> Expr.mk_const_s ctx "NumberType" type_sort
-	| StringType            -> Expr.mk_const_s ctx "StringType" type_sort
-  | ObjectType            -> Expr.mk_const_s ctx "ObjectType" type_sort
-	| ReferenceType         -> Expr.mk_const_s ctx "ReferenceType" type_sort
-	| ObjectReferenceType   -> Expr.mk_const_s ctx "ObjectReferenceType" type_sort
-	| VariableReferenceType -> Expr.mk_const_s ctx "VariableReferenceType" type_sort
-	| ListType              -> Expr.mk_const_s ctx "ListType" type_sort
-	| TypeType              -> Expr.mk_const_s ctx "TypeType" type_sort
+	| UndefinedType         -> Arithmetic.Integer.mk_numeral_i ctx 0
+	| NullType              -> Arithmetic.Integer.mk_numeral_i ctx 1
+	| EmptyType             -> Arithmetic.Integer.mk_numeral_i ctx 2
+	| NoneType              -> Arithmetic.Integer.mk_numeral_i ctx 3
+  | BooleanType           -> Arithmetic.Integer.mk_numeral_i ctx 4
+	| IntType               -> Arithmetic.Integer.mk_numeral_i ctx 5
+  | NumberType            -> Arithmetic.Integer.mk_numeral_i ctx 6
+	| StringType            -> Arithmetic.Integer.mk_numeral_i ctx 7
+  | ObjectType            -> Arithmetic.Integer.mk_numeral_i ctx 8
+	| ReferenceType         -> Arithmetic.Integer.mk_numeral_i ctx 9
+	| ObjectReferenceType   -> Arithmetic.Integer.mk_numeral_i ctx 10
+	| VariableReferenceType -> Arithmetic.Integer.mk_numeral_i ctx 11
+	| ListType              -> Arithmetic.Integer.mk_numeral_i ctx 12
+	| TypeType              -> Arithmetic.Integer.mk_numeral_i ctx 13
 
-(** Encode JSIL constants as Z3 constants of sort Arithmetic.Real *)
+(** Encode JSIL constants as Z3 numerical constants *)
 let encode_constant ctx constant =
-	let float_sort = sort_of_type ctx NumberType in
-	match constant with
-	| Min_float -> Expr.mk_const_s ctx "Min_float" float_sort
-	| Max_float -> Expr.mk_const_s ctx "Max_float" float_sort
-	| Random    -> Expr.mk_const_s ctx "Random" float_sort
-	| E         -> Expr.mk_const_s ctx "E" float_sort
-	| Ln10      -> Expr.mk_const_s ctx "Ln10" float_sort
-	| Ln2       -> Expr.mk_const_s ctx "Ln2" float_sort
-	| Log2e     -> Expr.mk_const_s ctx "Log2e" float_sort
-	| Log10e    -> Expr.mk_const_s ctx "Log10e" float_sort
-	| Pi        -> Expr.mk_const_s ctx "Pi" float_sort
-	| Sqrt1_2   -> Expr.mk_const_s ctx "Sqrt1_2" float_sort
-	| Sqrt2     -> Expr.mk_const_s ctx "Sqrt2" float_sort
+	let value =
+		(match JSIL_Interpreter.evaluate_constant constant with
+		| Num v -> v
+		| _     -> raise (Failure "SMT encoding: Unknown constant")) in
+	Arithmetic.Real.mk_numeral_s ctx (string_of_float value)
 
-(** Encode JSIL literals as constants of the corresponding sort *)
+(** Encode strings as Z3 numerical constants *)
+let str_codes = Hashtbl.create 1000
+let str_counter = ref 0
+let encode_string ctx str =
+	try Hashtbl.find str_codes str with
+	| Not_found ->
+		(* New string: add it to the hashtable *)
+		let z3_code = Arithmetic.Integer.mk_numeral_i ctx !str_counter in
+		str_counter := !str_counter + 1;
+		Hashtbl.add str_codes str z3_code;
+		z3_code
+
+(** Encode JSIL literals as Z3 numerical constants *)
 let encode_literal ctx lit =
 	match lit with
-	| Undefined  -> Expr.mk_const_s ctx "Undefined" (sort_of_type ctx UndefinedType)
-	| Null       -> Expr.mk_const_s ctx "Null" (sort_of_type ctx NullType)
-	| Empty      -> Expr.mk_const_s ctx "Empty" (sort_of_type ctx EmptyType)
+	| Undefined  -> Arithmetic.Integer.mk_numeral_i ctx 0
+	| Null       -> Arithmetic.Integer.mk_numeral_i ctx 1
+	| Empty      -> Arithmetic.Integer.mk_numeral_i ctx 2
 	| Constant c -> encode_constant ctx c
 	| Bool b     -> Boolean.mk_val ctx b
 	| Integer i  -> Arithmetic.Integer.mk_numeral_i ctx i
-	| Num n      -> Arithmetic.Real.mk_numeral_s ctx (string_of_float n)
-	| String s   -> Expr.mk_const_s ctx s (sort_of_type ctx StringType)
-  | Loc l      -> Expr.mk_const_s ctx l (sort_of_type ctx ObjectType)
-  | Type t     -> encode_type ctx t
+	| Num n      ->
+		if (n = (snd (modf n)))
+			then Arithmetic.Integer.mk_numeral_i ctx (int_of_float n)
+			else Arithmetic.Real.mk_numeral_s ctx (string_of_float n)
+	| String s   -> encode_string ctx s
+	| Loc l      -> encode_string ctx ("$l" ^ l)
+	| Type t     -> encode_type ctx t
 	| _          -> raise (Failure "SMT encoding: Construct not supported yet!")
 
 (** Encode JSIL binary operators *)
@@ -91,13 +111,9 @@ let rec encode_logical_expression ctx gamma e =
 	let ele = encode_logical_expression ctx gamma in
 	(match e with
 	| LLit lit              -> encode_literal ctx lit
-	| LNone                 -> Expr.mk_const_s ctx "None" (sort_of_type ctx NoneType)
-	| LVar var              ->
-		let var_type =
-			(try Hashtbl.find gamma var with
-			| _ -> raise (Failure (Printf.sprintf "Logical variables must be typed. Could not find type of %s.\n" var))) in 
-		Expr.mk_const_s ctx var (sort_of_type ctx var_type)
-	| ALoc aloc             -> Expr.mk_const_s ctx aloc (sort_of_type ctx ObjectType)
+	| LNone                 -> Arithmetic.Integer.mk_numeral_i ctx 3
+	| LVar var
+	| ALoc var              -> Arithmetic.Integer.mk_const_s ctx var
 	| PVar _                -> raise (Failure "Program variable in pure formula: FIRE")
 	| LBinOp (le1, op, le2) -> encode_binop ctx op (ele le1) (ele le2)
 	| LUnOp (op, le)        -> encode_unop ctx op (ele le)
@@ -113,11 +129,9 @@ let rec encode_pure_formula ctx gamma a =
 	match a with
 	| LNot a             -> Boolean.mk_not ctx (f a)
 	| LEq (le1, le2)     ->
-		let le1_sort = Expr.get_sort (fe le1) in
-		let le2_sort = Expr.get_sort (fe le2) in
-		(if Sort.equal le1_sort le2_sort
+		if (type_of_logic_expr gamma le1) = (type_of_logic_expr gamma le2)
 			then Boolean.mk_eq ctx (fe le1) (fe le2)
-			else Boolean.mk_false ctx)
+			else Boolean.mk_false ctx
 	| LLess (le1, le2)   -> Arithmetic.mk_lt ctx (fe le1) (fe le2)
 	| LLessEq (le1, le2) -> Arithmetic.mk_le ctx (fe le1) (fe le2)
 	| LStrLess (_, _)    -> raise (Failure ("I don't know how to do string comparison in Z3"))
@@ -143,5 +157,8 @@ let check_entailment left_as right_as gamma =
 			left_as in 
 	let solver = (Solver.mk_solver ctx None) in
 	Solver.add solver (left_as @ [ right_as_or ]);
+	List.iter
+		(fun expr -> Printf.printf "Z3 Expression: %s\n" (Expr.to_string expr))
+		(left_as @ [ right_as_or ]);
 	Printf.printf "I checked what I had to check\n";
 	(if (Solver.check solver []) != Solver.SATISFIABLE then true else false)
