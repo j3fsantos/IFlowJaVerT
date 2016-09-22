a9add0f346131dd135c7661394d0036d3ba52cd8
pmaksimo <p.maksimovic@imperial.ac.uk>
2016-09-22 18:33:13 +0100

    date

--- src/pulp/SJSIL/translation_runtime/harness.js
+++ src/pulp/SJSIL/translation_runtime/harness.js
@@ -269,3 +269,33 @@ function compareArray(a, b) {
 	  }
 	  return true;
 	}
+
+function assertRelativeDateMs(date, expectedMs) {
+	  var actualMs = date.valueOf();
+	  var localOffset = date.getTimezoneOffset() * 60000;
+
+	  if (actualMs - localOffset !== expectedMs) {
+	    $ERROR(
+	      'Expected ' + date + ' to be ' + expectedMs +
+	      ' milliseconds from the Unix epoch'
+	    );
+	  }
+	}
+
+var HoursPerDay = 24;
+var MinutesPerHour = 60;
+var SecondsPerMinute = 60;
+
+var msPerDay = 86400000;
+var msPerSecond = 1000;
+var msPerMinute = 60000;
+var msPerHour = 3600000;
+
+var date_1899_end = -2208988800001;
+var date_1900_start = -2208988800000;
+var date_1969_end = -1;
+var date_1970_start = 0;
+var date_1999_end = 946684799999;
+var date_2000_start = 946684800000;
+var date_2099_end = 4102444799999;
+var date_2100_start = 4102444800000;
