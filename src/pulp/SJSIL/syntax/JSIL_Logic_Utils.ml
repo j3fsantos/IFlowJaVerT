e5a56f35921be61462d041bbcb484f63e4eea34d
jfaustin <josesantos@Joses-MacBook-Pro.local>
2016-10-03 19:14:01 +0100

    the real getProperty apparently working

--- src/pulp/SJSIL/syntax/JSIL_Logic_Utils.ml
+++ src/pulp/SJSIL/syntax/JSIL_Logic_Utils.ml
@@ -509,7 +509,9 @@ let init_substitution2 vars les =
 	loop vars les; 
 	subst
 
-		
+
+
+
 (**
  subst1 after subst2    
 **)
@@ -528,6 +530,12 @@ let copy_substitution subst = Hashtbl.copy subst
 let extend_subst subst var v = 
 	Hashtbl.replace subst var v
 
+
+let extend_substitution subst vars les = 
+	List.iter2 
+		(fun v le -> Hashtbl.replace subst v le)
+		vars 
+		les
 
 
 let filter_vars vars ignore_vars : string list = 
