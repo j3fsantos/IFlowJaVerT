d49b91a3560c3f3a4522720763ae6f824b42e60e
pmaksimo <p.maksimovic@imperial.ac.uk>
2017-01-02 00:55:47 +0000

    pretty

--- environment/function_test.js
+++ environment/function_test.js
@@ -1,7 +1,9 @@
 /**
  @toprequires (emp)
- @topensures (scope(y : 3) * scope(g: #g) * scope(f: #f) *
- 	fun_obj(g, #g, #g_proto) * fun_obj(f, #f, #f_proto) * scope(w: 7))
+ @topensures (
+ 	scope(g : #g) * fun_obj(g, #g, #g_proto) * 
+ 	scope(f : #f) * fun_obj(f, #f, #f_proto) * 
+ 	scope(y :  3) * scope(w : 7))
 */
 var y = 3;
 
@@ -10,12 +12,10 @@ var y = 3;
 	@id  g
 	@rec false
 
-	@pre  (scope(g: #g) * scope(f: #f) * scope(y : #y) * (z == #z) * types(#y : $$number_type, #z : $$number_type))
-	@post (scope(g: #g) * scope(f: #f) * scope(y : #y) * (ret == (#z + #y)))
+	@pre  (scope(g : #g) * scope(f : #f) * scope(y : #y) * (z == #z) * types(#y : $$number_type, #z : $$number_type))
+	@post (scope(g : #g) * scope(f : #f) * scope(y : #y) * (ret == (#z + #y)))
 */
-var g = function (z) {
-	return z + y
-};
+var g = function (z) { return z + y };
 
 
 /**
@@ -25,6 +25,6 @@ var g = function (z) {
 	@pre  (scope(g: #g) * scope(f: #f) * scope(y : #y) * (u == #u) * types(#u : $$number_type))
 	@post (scope(g: #g) * scope(f: #f) * scope(y : #y) * (ret == (#u + 1)))
 */
-function f (u) { return u + 1}
+function f (u) { return u + 1 }
 
 var w = f(g(3))
