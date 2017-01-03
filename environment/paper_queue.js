d5d904f28c678ee92e3f5c768907003a39d021eb
jfaustin <josesantos@Joses-MacBook-Pro.local>
2017-01-03 01:55:33 +0000

    paper_queue.js

--- environment/paper_queue.js
+++ environment/paper_queue.js
@@ -8,14 +8,14 @@ pred Object (l, proto) :
 	((l, "@extensible") -> $$t);
 
 pred NodePrototype(np) :
-	StandardObject(np) * 
+	StandardObject(np) *
 	dataField(np, "push", #push) *
 	fun_obj(push, #push, #push_proto);
 
 pred Node(n, pri, elt, next, node_proto) :
-	Object(n, node_proto) * 
-	dataField(n, "pri",  pri) * 
-    dataField(n, "elt",  elt) * 
+	Object(n, node_proto) *
+	dataField(n, "pri",  pri) *
+    dataField(n, "elt",  elt) *
     dataField(n, "next", next) *
     types(pri : $$int_type, elt : $$string_type, node_proto : $$object_type);
 
@@ -23,110 +23,78 @@ pred Queue(q, node_proto, max_pri) :
 	(q == $$null) * (max_pri == -1);
 
 pred Queue(q, node_proto, max_pri) :
-	Node(q, #pri, #elt, #next, node_proto) * 
+	Node(q, #pri, #elt, #next, node_proto) *
 	Queue(#next, node_proto, #pri) *
 	(#pri <= max_pri) *
 	types(node_proto : $$object_type, max_pri : $$int_type);
 */
 
+var counter = 0;
+
 /**
-	@id  newNodeConstructor
+	@id  Node
 	@rec false
-	
-	@pre  (emp)
-	@post (
-		dataField(ret, "nc", #nc) * 
-		datafield(ret, "gc", #gc) *
-		fun_obj(Node, #nc, #node_proto) *
-		fun_obj(getCounter, #gc, #gc_proto) *
-		NodePrototype(#node_proto)
+
+	@pre (
+	   	scope(counter: #c) * types(#c : $$int_type) *
+	   	(pri == #pri) * (elt == #elt) * types(#pri: $$int_type, #elt: $$string_type) *
+	   	((this, "pri") -> None) * ((this, "elt") -> None) * ((this, "next") -> None) *
+	   	Object(this, #node_proto) * NodePrototype(#node_proto)
 	)
+	@post (
+	   		scope(counter: #c + 1) *
+	   		Node(this, #pri, #elt, $$null, #node_proto) *
+	   		NodePrototype(#node_proto))
 */
-function newNodeConstructor () {
-	var counter = 0;
-	
-	/**
-	   @id  Node
-	   @rec false
-	
-	   @pre (
-	   		scope(counter: #c) * types(#c : $$int_type) *
-	   		(pri == #pri) * (elt == #elt) * types(#pri: $$int_type, #elt: $$string_type) *  
-	   		((this, "pri") -> None) * ((this, "elt") -> None) * ((this, "next") -> None) * 
-	   		Object(this, #node_proto) * NodePrototype(#node_proto)
-	   )
-	   @post (
-	   		 scope(counter: #c + 1) *
-	   		 Node(this, #pri, #elt, $$null, #node_proto) *
-	   		 NodePrototype(#node_proto))
-	*/
-	var Node = function (pri, elt) {
-	   this.pri = pri; this.elt = elt; this.next = null;
-	   counter++
-	}
-	
-	/**
-		@id  push
-		@rec true
-		
-		@pre (
-			(q == #q) * 
-			Node(this, #pri, #elt, $$null, #node_proto) *
-			Queue(#q, #node_proto, #pri_q) * 
-			NodePrototype(#node_proto) *
-			scope(Node: #node) * 
-			fun_obj(Node, #nc, #node_proto)
-		)
-		@post (
-			Queue(ret, #node_proto, #pri_f) * 
-			NodePrototype(#node_proto) *
-			scope(Node: #node) * 
-			fun_obj(Node, #nc, #node_proto)
-		)
-	*/
-	Node.prototype.push = function (q) {
-		/**
-			@unfold Queue(#q, #node_proto, #pri_q)
-		*/
-		if (q === null) {
-			/**
-			   @fold Queue(this, #node_proto, #pri)
-			*/
-			return this
-		} 
-		else 
-			if (this.pri >= q.pri) {
-				this.next = q;
-				/**
-				   @fold Queue(this, #node_proto, #pri)
-				*/
-				return this
-			} 
-			else 
-			{
-				var tmp = this.push (q.next);
-				q.next = tmp;
-				/**
-					@fold Queue(#q, #node_proto, #pri_q)
-				*/
-				return q
-			}
-	}
-
-
-   /**
-   	@id  getCounter
-   	@rec false
-
-   	@pre  (scope(counter: #c) * types(#c : $$int_type))
-   	@post (scope(counter: #c) * (ret == #c))
-   */
-   var getCounter = function () { return counter; };
-   
-   return { nc: Node, gc: getCounter }
+var Node = function (pri, elt) {
+	this.pri = pri; this.elt = elt; this.next = null;
+	counter++
 }
 
+/**
+	@id  push
+	@rec true
 
-var o = newNodeConstructor ();
-var Node = o.nc;
-var getCounter = o.getCounter;
+	@pre (
+		(q == #q) *
+		Node(this, #pri, #elt, $$null, #node_proto) *
+		Queue(#q, #node_proto, #pri_q) *
+		NodePrototype(#node_proto) *
+		scope(Node: #node) *
+		fun_obj(Node, #nc, #node_proto)
+	)
+	@post (
+		Queue(ret, #node_proto, #pri_f) *
+		NodePrototype(#node_proto) *
+		scope(Node: #node) *
+		fun_obj(Node, #nc, #node_proto)
+	)
+*/
+Node.prototype.push = function (q) {
+	/**
+		@unfold Queue(#q, #node_proto, #pri_q)
+	*/
+	if (q === null) {
+		/**
+			@fold Queue(this, #node_proto, #pri)
+		*/
+		return this
+	}
+	else
+		if (this.pri >= q.pri) {
+			this.next = q;
+			/**
+				@fold Queue(this, #node_proto, #pri)
+			*/
+			return this
+		}
+		else
+		{
+			var tmp = this.push (q.next);
+			q.next = tmp;
+			/**
+				@fold Queue(#q, #node_proto, #pri_q)
+			*/
+			return q
+		}
+}
