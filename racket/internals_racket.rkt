7c6f2e55fec1c0647aac091a1fede9891fc020a5
jfaustin <josesantos@dyn1185-50.wlan.ic.ac.uk>
2017-02-21 16:10:54 +0000

    toUpperCase

--- racket/internals_racket.rkt
+++ racket/internals_racket.rkt
@@ -15,7 +15,14 @@
                  ret))))
       (list 'normal ret))))
 
+
+(define (toUpperCase s)
+  (list 'normal (string-upcase s)))
+
+
 (define (register-racket-methods hp)
-  (register-js-builtin-method "String" "startsWith" starts-with? hp))
+  (register-js-builtin-method "String" "startsWith" starts-with? hp)
+  (register-js-builtin-method "String" "toUpperCase" toUpperCase hp))
+
 
 (provide register-racket-methods)
