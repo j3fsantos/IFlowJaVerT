e112396beba5d8ae696abdbaf8a6e8807077b402
jfaustin <josesantos@Joses-MacBook-Pro.local>
2017-02-21 00:22:31 +0000

    racket bypassing of jsil builtins

--- racket/mem_model.rkt
+++ racket/mem_model.rkt
@@ -1,4 +1,4 @@
-#lang rosette
+#lang racket
 
 ;;
 ;; Support for list "hash-tables"
@@ -724,7 +724,6 @@
 ;; Internal / Builtin Procedure Implemented in Racket 
 ;;
 
-
 (define racket-js-implementations (make-hash))
 
 (define (has-racket-implementation? proc-name)
@@ -733,11 +732,41 @@
 (define (get-racket-implementation proc-name)
   (hash-ref racket-js-implementations proc-name))
 
+(define (create-new-function-obj hp function-name)
+  (let ((fun-loc (get-new-loc)))
+    (mutate-heap hp fun-loc "@call" function-name)
+    (mutate-heap hp fun-loc "@construct" function-name)
+    (mutate-heap hp fun-loc "@scope" '(jsil-list))
+    (mutate-heap hp fun-loc "@proto" '$lfun_proto)
+    (mutate-heap hp fun-loc "@class" "Function")
+    (mutate-heap hp fun-loc "@extensible" #t)
+    fun-loc))
 
 (define (register-js-builtin-method builtin-obj-name method-name racket-method hp)
-  (let* ((builtin-obj-loc (heap-get hp jsglobal builtin-obj-name))
-         (method-obj-loc (heap-get hp builtin-obj-loc method-name)))
-    hp))
+  (println "inside register-js-builtin-method")
+  (println (format "checking the object at ~v" jsglobal))
+  (let* ((builtin-obj-desc (heap-get hp jsglobal builtin-obj-name))
+         (builtin-obj-loc (second (first builtin-obj-desc)))
+         (builtin-obj
+          (begin
+            (println (format "here is the descriptor ~v" builtin-obj-desc))
+            (println (format "checking the object at ~v" builtin-obj-loc))
+            (heap-get-obj hp builtin-obj-loc)))
+         (method-obj-loc (lht-ref builtin-obj method-name))
+         (fresh-function-name (symbol->string (gensym "internal-function-"))))
+    ;; put the racket method in the hashtable
+    (hash-set! racket-js-implementations fresh-function-name racket-method)
+    ;;
+    (if (eq? method-obj-loc empty)
+        ;; the method does not exist - we need to create it
+        (let ((method-obj-loc (create-new-function-obj hp fresh-function-name)))
+          (mutate-heap hp builtin-obj-loc method-name method-obj-loc))
+        ;; the method already exists and we are just going to override it with a racket implementation
+        (begin
+          (mutate-heap hp method-obj-loc "@call" fresh-function-name)
+          (mutate-heap hp method-obj-loc "@construct" fresh-function-name)))))
+
+(provide has-racket-implementation? get-racket-implementation register-js-builtin-method)
     
 
 
