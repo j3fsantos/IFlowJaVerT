60aad30b93e4ccff9694a727e94822598877e7cf
Julian Dolby <juliandolby@gmail.com>
2017-02-27 20:00:32 -0500

    replace test

--- tests/symbolic/primitives/symbolic-prim-replace.js
+++ tests/symbolic/primitives/symbolic-prim-replace.js
@@ -1,14 +1,27 @@
 var v = 0;
-var s1 = "abcdefghijk";
+var s1 = jsil_make_symbolic_string();
 var s2 = jsil_make_symbolic_string();
+var s3 = jsil_make_symbolic_string();
 
-var s = s1.replace(s2, "xyz");
-
-if ("a" === s.charAt(0)) {
+if (s1.length > 0 && "a" === s1.charAt(0)) {
     v++;
 }
 
-jsil_assert(v === 1)
+if (s2.length > 0) {
+    v++;
+}
+
+var s = s1.replace(s2, "xyz");
+
+if (s.length > 0 && "x" === s.charAt(0)) {
+    v++;
+}
+
+if (s === s3) {
+    v++;
+}
+
+jsil_assert(v === 4)
 
 
 
