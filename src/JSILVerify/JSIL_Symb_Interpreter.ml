055e94d3fa8a0ea33babc84ba065bcd278cff4fd
jfaustin <josesantos@dyn1190-214.wlan.ic.ac.uk>
2017-03-01 17:51:26 +0000

    fixing a problem with find_and_apply_spec

--- src/JSILVerify/JSIL_Symb_Interpreter.ml
+++ src/JSILVerify/JSIL_Symb_Interpreter.ml
@@ -374,12 +374,12 @@ let find_and_apply_spec prog proc_name proc_specs (symb_state : symbolic_state) 
 
 		print_debug (Printf.sprintf "Entering transform_symb_state.\n");
 
-		let merge_symb_state_with_single_post (symb_state : symbolic_state) (post : symbolic_state) ret_var ret_flag copy_flag : (symbolic_state * jsil_return_flag * jsil_logic_expr) list =
+		let merge_symb_state_with_single_post (symb_state : symbolic_state) (post : symbolic_state) ret_var ret_flag : (symbolic_state * jsil_return_flag * jsil_logic_expr) list =
 			print_debug (Printf.sprintf "Entering merge_symb_state_with_single_post.");
 			let post_makes_sense = compatible_pfs symb_state post subst in
 			if (post_makes_sense) then (
 				print_debug (Printf.sprintf "The post makes sense.");
-				let new_symb_state = if (copy_flag) then (copy_symb_state symb_state) else symb_state in
+				let new_symb_state = copy_symb_state symb_state in
 				let new_symb_state = Structural_Entailment.merge_symb_states new_symb_state post subst in
 				let ret_lexpr = store_get_var (get_store post) ret_var in
 				let ret_lexpr = JSIL_Logic_Utils.lexpr_substitution ret_lexpr subst false in
@@ -395,12 +395,12 @@ let find_and_apply_spec prog proc_name proc_specs (symb_state : symbolic_state) 
 		let symb_states_and_ret_lexprs =
 			(match spec.n_post with
 			| [] -> print_debug (Printf.sprintf "No postconditions found."); []
-			| [ post ] -> print_debug (Printf.sprintf "One postcondition found."); merge_symb_state_with_single_post symb_state post ret_var ret_flag false
+			| [ post ] -> print_debug (Printf.sprintf "One postcondition found."); merge_symb_state_with_single_post symb_state post ret_var ret_flag
 			| post :: rest_posts ->
 					print_debug (Printf.sprintf "Multiple postconditions found.");
-					let symb_states_and_ret_lexprs = List.map (fun post -> merge_symb_state_with_single_post symb_state post ret_var ret_flag true) rest_posts in
+					let symb_states_and_ret_lexprs = List.map (fun post -> merge_symb_state_with_single_post symb_state post ret_var ret_flag) rest_posts in
 					let symb_states_and_ret_lexprs =
-						(merge_symb_state_with_single_post symb_state post ret_var ret_flag false) :: symb_states_and_ret_lexprs in
+						(merge_symb_state_with_single_post symb_state post ret_var ret_flag) :: symb_states_and_ret_lexprs in
 					let symb_states_and_ret_lexprs = List.concat symb_states_and_ret_lexprs in
 					symb_states_and_ret_lexprs) in
 		symb_states_and_ret_lexprs in
