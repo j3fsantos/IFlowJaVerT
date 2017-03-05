a155b781ca08ba40eff0551135351e43ef6332cc
Cesar Roux Dit Buisson <cesar.roux-dit-buisson13@imperial.ac.uk>
2017-03-05 18:39:59 +0000

    Add one of the example specs to sanitiseImg
    Trying to get a feel for what these specs will look like. The one I've added is the one that has a proof sketch expanded in the DOM paper. I skipped the val predicate and did a simpler case of a single text node, this may be too inaccurate.
    
    The list part of the specification, where I try to describe the shape of the attributes of the element node may pose issues or be in the incorrect format.

--- src/JaVerT/DOM/examples/ImageSanitise/sanitise.js
+++ src/JaVerT/DOM/examples/ImageSanitise/sanitise.js
@@ -1,3 +1,45 @@
+/**
+	@onlyspec isBlackListed() :
+		pre: [[ scope(url: #s1) * scope(isB: #ignore) * isB(#s1) ]]
+		post: [[ scope(url: #s1) * scope(isB:1) * isB(#s1) ]]
+		outcome: normal;
+		pre: [[ scope(url: #s1) * scope(isB: #ignore) * (not isB(#s1)) ]]
+		post: [[ scope(url: #s1) * scope(isB:0) * (not isB(#s1)) ]]
+		outcome: normal;		
+*/
+
+/**
+	@id sanitise
+	@rec false
+
+	@pre (
+		scope(img: #n) * 
+		scope(cat: #s2) * 
+		scope(cache: #c) *
+		scope(url: #ignore1) * 
+		scope(isB: #ignore2) *
+		ElementNode(#n, #name, #attr, #children) *
+		(#attr = #attr1 @ #a) *
+		AttributeNode(#a, "src", {{#t}}) *
+		TextNode(#t, #s1) *
+		((#c, #t) -> 0) *
+		isB(#s1)
+	)
+	@post (
+		scope(img: #n) * 
+		scope(cat: #s2) * 
+		scope(cache: #c) *
+		scope(url: #s1) * 
+		scope(isB: 1) * 
+		ElementNode(#n, #name, #new_attr, #children) *
+		(#new_attr = #attr1 @ #a) *
+		AttributeNode(#a, "src", {{#tn}}) *
+		TextNode(#tn, #s2) *
+		((#c, #t) -> 1) *
+		isB(#s1)
+	)
+**/
+
 function sanitiseImg(img, cat){
 	var url = img.getAttribute("src");
 	if(url){
