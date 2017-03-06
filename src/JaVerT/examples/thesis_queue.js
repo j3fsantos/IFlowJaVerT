3d7d47341a754646c941b78b050f2d880bc7d4b6
Daiva Naudziuniene <daiva.naudziuniene@gmail.com>
2017-03-06 21:06:36 +0000

    Experimenting with thesis queue. Verifying enqueue without closure.

--- src/JaVerT/examples/thesis_queue.js
+++ src/JaVerT/examples/thesis_queue.js
@@ -23,17 +23,31 @@
 	(0 <# pri) *
 	types(pri : $$number_type, val : $$string_type, node_proto : $$object_type);
 
-  @pred Queue(q, node_proto, max_pri, length) :
-  	(q == $$null) * (max_pri == 0) * (length == 0) * types(max_pri : $$number_type, length : $$number_type),
+@pred Queue(q, node_proto, max_pri, length) :
+	(q == $$null) * (max_pri == 0) * (length == 0) * types(max_pri : $$number_type, length : $$number_type),
 
-  	Node(q, max_pri, #val, #next, node_proto) * (0 <# max_pri) *
-  	Queue(#next, node_proto, #pri, #len_q) * (#pri <=# max_pri) * (length == #len_q + 1) *
-  	types(q : $$object_type, node_proto : $$object_type, #pri : $$number_type, max_pri : $$number_type, length : $$number_type, #len_q : $$number_type);
+	Node(q, max_pri, #val, #next, node_proto) * (0 <# max_pri) *
+	Queue(#next, node_proto, #pri, #len_q) * (#pri <=# max_pri) * (length == #len_q + 1) *
+	types(q : $$object_type, node_proto : $$object_type, #pri : $$number_type, max_pri : $$number_type, length : $$number_type, #len_q : $$number_type);
+
+@pred PQPrototype(pqp, enqueue_loc) :
+	standardObject(pqp) *
+	dataField(pqp, "enqueue", enqueue_loc) *
+	fun_obj(enqueue, enqueue_loc, #enqueue_proto) *
+	((pqp, "_head") -> None);
+
+  @pred PQ(pq, head, pq_proto, node_proto, max_pri, length) :
+  	Object(pq, pq_proto) *
+  	dataField(pq, "_head",  head) *
+    Queue(head, node_proto, max_pri, length) *
+  	((pq, "enqueue") -> None) *
+  	types(pq_proto : $$object_type, node_proto : $$object_type, max_pri : $$number_type, length : $$number_type);
 */
 
 
-/* @id Module */
-var PriorityQueue = (function () {
+//var PriorityQueue;
+// /* @id Module */
+//PriorityQueue = (function () {
 
   /**
   	@id  Node
@@ -48,7 +62,7 @@ var PriorityQueue = (function () {
 
   	@post (
   	   		Node(this, #pri, #val, $$null, #node_proto) *
-  	   		NodePrototype(#node_proto, #insert_loc))
+  	   		NodePrototype(#node_proto, #insert_loc) * (ret == $$empty) )
   */
    var Node = function (pri, val) {
       this.pri = pri; this.val = val; this.next = null;
@@ -105,33 +119,86 @@ var PriorityQueue = (function () {
       return q
    }
 
-   /* @id PriorityQueue */
-   var module = function () {
+   /**
+    @id  PriorityQueue
+
+    @pre (
+        ((this, "_head") -> None) *
+        ((this, "enqueue") -> None) *
+        Object(this, #pq_proto) * PQPrototype(#pq_proto, #enqueue_loc) *
+        NodePrototype(#node_proto, #insert_loc)
+    )
+    @post (
+          PQ(this, $$null, #pq_proto, #node_proto, 0, 0) *
+          PQPrototype(#pq_proto, #enqueue_loc)) *
+          NodePrototype(#node_proto, #insert_loc)
+   */
+   var PriorityQueue = function () {
+       /** @fold Queue($$null, #node_proto, 0, 0) */
       this._head = null;
    };
 
-   /* @id enqueue */
-   module.prototype.enqueue = function(pri, val) {
+   /**
+     @id enqueue
+
+      @pre (
+        (pri == #npri) *
+        (val == #nval) *
+        (0 <# #npri) *
+        scope(Node: #n) *
+        fun_obj(Node, #n, #node_proto) *
+        PQ(this, #head, #pq_proto, #node_proto, #pri_q, #length) *
+        PQPrototype(#pq_proto, #enqueue_loc) *
+        NodePrototype(#node_proto, #insert_loc) *
+        types(#pri_q : $$number_type, #length : $$number_type, #npri : $$number_type, #pri_q : $$number_type, #nval: $$string_type) *
+        (#pri_q <=# #npri)
+      )
+      @post (
+        PQ(this, #new_head, #pq_proto, #node_proto, #npri, #length + 1) *
+        PQPrototype(#pq_proto, #enqueue_loc) *
+        NodePrototype(#node_proto, #insert_loc)
+      )
+
+      @pre (
+        (pri == #npri) *
+        (val == #nval) *
+        (0 <# #npri) *
+        scope(Node: #n) *
+        fun_obj(Node, #n, #node_proto) *
+        PQ(this, #head, #pq_proto, #node_proto, #pri_q, #length) *
+        PQPrototype(#pq_proto, #enqueue_loc) *
+        NodePrototype(#node_proto, #insert_loc) *
+        types(#pri_q : $$number_type, #length : $$number_type, #npri : $$number_type, #pri_q : $$number_type, #nval: $$string_type, #head: $$object_type) *
+        (#npri <# #pri_q)
+      )
+      @post (
+        PQ(this, #head, #pq_proto, #node_proto, #pri_q, #length + 1) *
+        PQPrototype(#pq_proto, #enqueue_loc) *
+        NodePrototype(#node_proto, #insert_loc)
+      )
+
+   */
+   PriorityQueue.prototype.enqueue = function(pri, val) {
       var n = new Node(pri, val);
       this._head = n.insertToQueue(this._head);
    };
 
-   /* @id dequeue */
-   module.prototype.dequeue = function () {
-      if (this._head === null) {
-         throw new Error("Queue is empty");
-      }
+//   /* @id dequeue */
+//   PriorityQueue.prototype.dequeue = function () {
+//      if (this._head === null) {
+//         throw new Error("Queue is empty");
+//      }
 
-      var first = this._head;
-      this._head = this._head.next;
-      return {pri: first.pri, val: first.val};
-   };
+//      var first = this._head;
+//      this._head = this._head.next;
+//      return {pri: first.pri, val: first.val};
+//   };
 
-   return module;
-})();
+//   return module;
+//})();
 
 var q = new PriorityQueue();
 q.enqueue(1, "last");
 q.enqueue(3, "bar");
 q.enqueue(2, "foo");
-var r = q.dequeue();
+//var r = q.dequeue();
