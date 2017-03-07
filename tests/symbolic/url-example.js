03f13f2be46233080c023661a80357c57f477553
Julian Dolby <juliandolby@gmail.com>
2017-03-07 10:56:34 -0500

    beginnings of url example

---
+++ tests/symbolic/url-example.js
@@ -0,0 +1,27 @@
+var base = "https://randombank.ru/api/service/account";
+
+// read from DOM
+var name = jsil_make_symbolic_string();
+
+// check name non-empty
+if (name.length > 0) {
+    
+    base += ("?name="+name);
+
+    var account = jsil_make_symbolic_string();
+    var useAcct = (account.length == 10 && account.startsWith("0123"));
+    if (useAcct) {
+	base += ("?acct="+account);
+    }
+
+    var id = jsil_make_symbolic_string();
+    var useId = (account.id == 8 && id.startsWith("id"));
+    if (useId) {
+	base += ("?id"+id);
+    }
+}
+
+base
+
+jsil_assert(base.includes("?acct") || base.includes("?id"))
+
