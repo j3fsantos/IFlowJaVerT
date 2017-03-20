3249610178cfe91bc210eeea712d4d5fcc74de3c
Julian Dolby <juliandolby@gmail.com>
2017-03-20 08:25:09 -0400

    merge

--- tests/symbolic/url-example.js
+++ tests/symbolic/url-example.js
@@ -1,27 +1,12 @@
-var base = "https://randombank.ru/api/service/account";
+var url = jsil_make_symbolic_string();
 
-// read from DOM
-var name = jsil_make_symbolic_string();
-
-// check name non-empty
-if (name.length > 0) {
-    
-    base += ("?name="+name);
-
-    var account = jsil_make_symbolic_string();
-    var useAcct = (account.length == 10 && account.startsWith("0123"));
-    if (useAcct) {
-	base += ("?acct="+account);
-    }
-
-    var id = jsil_make_symbolic_string();
-    var useId = (id.length == 8 && id.startsWith("id"));
-    if (useId) {
-	base += ("?id"+id);
-    }
+if (url != "") {
+    if (url.substring(0,14) == '#access_token=') {
+        url = "https://api.instagram.com/v1/users/self/?access_token="+url.substring(14, url.length);
+    } 
 }
 
-base
+jsil_assert( url.includes("access_token") );
 
-jsil_assert(base.includes("?id"))
+
 
