54a9df442c99e3da5b97e0776e00470732874b04
Julian Dolby <juliandolby@gmail.com>
2017-03-28 12:38:52 -0400

    tests

--- tests/symbolic/symbolic-simple3.js
+++ tests/symbolic/symbolic-simple3.js
@@ -2,8 +2,8 @@ var f1 = jsil_make_symbolic_number (n1);
 var f2 = jsil_make_symbolic_number (n2); 
 
 var a = [ 1, 2, 3 ];
-var v1 = a[f1];
-var v2 = a[f2];
+var v1 = a[ (f1 == 0)? 0: (f1 == 1)?1: 2];
+var v2 = a[ (f2 == 0)? 0: (f2 == 1)?1: 2];
 
 jsil_assert ((v1 == v2));
 
