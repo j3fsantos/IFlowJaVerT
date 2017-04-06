a04a3ddb7213cf6821b9607f6e7b3ebe40a92c7a
Cesar Roux Dit Buisson <cesar.roux-dit-buisson13@imperial.ac.uk>
2017-04-06 11:40:24 +0100

    steps in double_unfold example

--- src/JaVerT/examples/double_unfold_1.js
+++ src/JaVerT/examples/double_unfold_1.js
@@ -36,6 +36,7 @@ function f (x, y) {
    /** @invariant dataField(x, "next", #x2)               */
    /** @unfold List(#x2, #dontcare2)                      */
    var a2 = x.value; 
-   /** @fold List(x, (#v1 :: (#v2 :: #values_a)))           */
+   /** @fold List(#x2, (#v2 :: #values_a))                */
+   /** @fold List(x, (#v1 :: (#v2 :: #values_a)))         */
    return a1 + a2
 }
