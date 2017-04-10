36cbde4e1eb1cd85084712a4142feacc840b2387
Beatrix De Wilde <beatrixdewilde@gmail.com>
2017-04-10 17:08:33 +0100

    Member check - field does not exist

--- src/JSILVerify/BiAbduction/Bi_Symbolic_State_Functions.ml
+++ src/JSILVerify/BiAbduction/Bi_Symbolic_State_Functions.ml
@@ -45,15 +45,31 @@ let abs_heap_find (symb_state : symbolic_state) (anti_frame : symbolic_state) lo
 		let msg = Printf.sprintf "Cannot decide if %s exists in object %s" (JSIL_Print.string_of_logic_expression e_field false) loc in
 			raise (Failure msg)
 
-let abs_heap_check_field_existence  (symb_state : symbolic_state) (anti_frame : symbolic_state) l e p_formulae gamma =
-	let _, _, pure_formulae, gamma, _ = symb_state in
-	let ret = abs_heap_find symb_state anti_frame l e in
-	match ret with
-	| (LUnknown, _) -> None, None
-	| (LNone, _) -> None, Some false
-	| (f_val, _) ->
+let abs_heap_check_field_existence  (symb_state : symbolic_state) (anti_frame : symbolic_state) loc e_field p_formulae gamma =
+	let heap, _, pure_formulae, gamma, _ = symb_state in
+	let anti_heap, _, anti_pure_formulae, _, _ = anti_frame in
+
+	let fv_list, default_val = try LHeap.find heap loc with _ -> [], LUnknown in
+	let _, field_val_pair, i_am_sure_the_field_does_not_exist = find_field loc fv_list e_field pure_formulae gamma in
+	
+	match field_val_pair, i_am_sure_the_field_does_not_exist, default_val with
+	| Some (_, f_val), _, _ -> 
 		if (Pure_Entailment.is_equal f_val LNone pure_formulae gamma) then
 			(Some f_val, Some false)
 			else (if (Pure_Entailment.is_different f_val LNone pure_formulae gamma) then
 				(Some f_val, Some true)
 				else (Some f_val, None))
+
+	| None, true, def when (not (def = LNone))  -> 
+		let v = LVar (fresh_lvar ()) in 
+		heap_put_fv_pair heap loc e_field v;
+		heap_put_fv_pair anti_heap loc e_field v;
+		(Some v, None)
+
+	| Some (_, LNone), _, _
+	| None, true, LNone  ->	
+		(Some LNone, Some false)
+
+	| None, false, _ ->
+		let msg = Printf.sprintf "Cannot decide if %s exists in object %s" (JSIL_Print.string_of_logic_expression e_field false) loc in
+			raise (Failure msg)
