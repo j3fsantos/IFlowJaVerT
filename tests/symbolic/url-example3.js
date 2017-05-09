ed52e2742ab052bb09e54a0ae9794006a4c35534
José Santos <josesantos@dyn210-249.tp.ph.ic.ac.uk>
2017-05-09 17:13:08 +0100

    more example

--- tests/symbolic/url-example3.js
+++ tests/symbolic/url-example3.js
@@ -1,24 +1,28 @@
-var url = jsil_make_symbolic_string(url);
+var url = "banana"; 
 var id = jsil_make_symbolic_string(id);
 
 // read from DOM
 var name = jsil_make_symbolic_string(name);
 var account = jsil_make_symbolic_string(account);
 
+//if (!url.includes("?")) { throw "bananas" } 
+
 // check name non-empty
-if ((name.length > 0)) {  
+if ((name.length > 0) && (! name.includes("?"))) {  
     url += ("?name="+name);
 }
 
 // check valid account
 var useAcct = (account.length == 10 && account.startsWith("0123"));
 if (useAcct) {
-	url += ("?acct="+account);
+    url += ("?acct="+account);
 }
-
 
 // check valid id 
 var useId = (id.length == 8 && id.startsWith("id"));
 if (useId) { url += ("?id"+id); }
+ 
+jsil_assert( (!useAcct && !useId) && (url.includes("?id") || url.includes("?acct")));
 
-jsil_assert( url.includes("?id") || url.includes("?acct"));
+
+
