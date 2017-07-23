72c110a4f8eedaf006284a950b6609f17c175301
pmaksimo <p.maksimovic@imperial.ac.uk>
2017-07-24 01:40:10 +0200

    test.sh

--- test.sh
+++ test.sh
@@ -2,16 +2,16 @@
 
 # Bash array format: ("one" "two" "three")
 # JS Files to test
-declare -a jsfiles=("priority_queue" "counter1"  "map" "bst" "sort" "counter2" "function_test1_fail" "function_test3" "DOM/new_DOM" "DOM/sanitiseImg" "closure1" "closure2")
+declare -a jsfiles=("kv-map" "map" "priority_queue" "bst" "sort" "counter1" "counter2" "function_test1_fail" "function_test3" "closure1" "closure2")
 # JSIL Files to test
-declare -a jsilfiles=("javert_internal_functions" "internal_functions_full")
+declare -a jsilfiles=("javert_internal_functions")
 
 echo "Testing js files"
 echo "----------------"
 for f in "${jsfiles[@]}"
 do
 	time {
-	START=$(date +%s%N)
+	echo "Next file: $f.js"
 	./js2jsil.native -file $f.js -logic &> /dev/null
 	rc=$?; if [[ $rc != 0 ]]; then echo "Failed js2jsil on $f"; fi
 	res=$(./jsilverify.native -file $f.jsil -js | tail -n1)
