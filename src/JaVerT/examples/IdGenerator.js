68fd8640fdbf97e857c4f3a65645b541c14d8976
pmaksimo <p.maksimovic@imperial.ac.uk>
2017-07-28 00:47:19 +0200

    real IdGenerator, updating test.sh

---
+++ src/JaVerT/examples/IdGenerator.js
@@ -0,0 +1,53 @@
+/**
+@pred idGenerator(ig, c_val, prefix) :
+   standardObject(ig) * types (c_val: $$number_type, prefix: $$string_type) *
+   dataField(ig, "getId", #gni) * fun_obj(getId, #gni, #gni_proto, #gni_sc) *
+   dataField(ig, "reset",  #ri) * fun_obj(reset,  #ri,  #ri_proto,  #ri_sc) *
+   closure(count: c_val, prefix: prefix; getId: #gni_sc, reset: #ri_sc);
+*/
+
+/**
+	@toprequires (emp)
+	@topensures (
+	   scope(makeIdGen: #mig) *
+	   scope(ig1: #ig1) *
+	   scope(ig2: #ig2) *
+	   fun_obj(makeIdGen, #mig, #mig_proto, #mig_sc) *
+	   idGenerator(#ig1, 1, "foo") *
+	   idGenerator(#ig2, 0, "bar") * 
+	   scope(id1: #id1) *
+	   (#id1 == "foo_id_0"))
+*/
+
+
+/**
+	@id   makeIdGen
+	@pre  ((prefix == #prefix) * types(#prefix: $$string_type))
+	@post (idGenerator(ret, 0, #prefix))
+*/
+var makeIdGen = function (prefix) { 
+
+	var count = 0; 
+
+	/**
+		@id   getId
+		@pre  (scope(count: #c)     * scope(prefix: #prefix) * types(#c: $$number_type, #prefix: $$string_type)) 
+		@post (scope(count: (#c+1)) * scope(prefix: #prefix) * (ret == (#prefix ++ "_id_" ++ num_to_string(#c))))
+	*/
+	var getId = function () { return prefix + "_id_" + (count++) }; 
+
+	/**
+		@id   reset
+		@pre  scope(count: #c)
+		@post scope(count: 0)
+	*/
+	var reset = function () { count = 0 }; 
+
+	return { getId: getId, reset: reset }
+};
+
+
+var ig1 = makeIdGen ("foo");
+var ig2 = makeIdGen ("bar");
+
+var id1 = ig1.getId();
--- test.sh
+++ test.sh
@@ -2,7 +2,7 @@
 
 # Bash array format: ("one" "two" "three")
 # JS Files to test
-declare -a jsfiles=("kv-map" "map" "priority_queue" "bst" "sort" "counter1" "counter2" "function_test1_fail" "function_test3" "closure1" "closure2")
+declare -a jsfiles=("kv-map" "IdGenerator" "priority_queue" "bst" "sort" "test262/switch-01" "test262/switch-02" "test262/try-catch-01" "test262/try-catch-02" "test262/try-catch-03" "counter1" "counter2" "map" "function_test1_fail" "function_test3" "closure1" "closure2")
 # JSIL Files to test
 declare -a jsilfiles=("javert_internal_functions")
 
