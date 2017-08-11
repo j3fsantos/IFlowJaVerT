472b4b3ee529f1bef2584abe6603f7a867998c7a
pmaksimo <p.maksimovic@imperial.ac.uk>
2017-08-11 15:27:49 -0400

    ok

--- src/JSILVerify/Simplifications.ml
+++ src/JSILVerify/Simplifications.ml
@@ -245,6 +245,13 @@ let rec reduce_expression (store : (string, jsil_logic_expr) Hashtbl.t)
 	| LBinOp (le1, CharCons, LCList []) -> LCList [ f le1 ]
 	| LBinOp (le1, CharCons, LLit (CList [])) -> LCList [ f le1 ]
 	| LBinOp (LEList le1, LstCat,  LEList le2) -> f (LEList (le1 @ le2))
+	| LBinOp (LLit (LList le1), LstCat, LLit (LList le2)) -> f (LLit (LList (le1 @ le2)))
+	| LBinOp (LLit (LList le1), LstCat, LEList le2) -> 
+			let le1 = List.map (fun x -> LLit x) le1 in
+			f (LEList (le1 @ le2))
+	| LBinOp (LEList le1, LstCat, LLit (LList le2)) -> 
+			let le2 = List.map (fun x -> LLit x) le2 in
+			f (LEList (le1 @ le2))
 	| LBinOp (LLit (String s1), StrCat, LLit (String s2)) -> f (LLit (String (s1 ^ s2)))
 	| LBinOp (LCList le1, CharCat, LCList le2) -> f (LCList (le1 @ le2))
 
