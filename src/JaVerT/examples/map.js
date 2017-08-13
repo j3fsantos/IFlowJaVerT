0a47f83f2ca1ecc1175bb88cf5ea7690526ae4ee
José Santos <j3fsantos@gmail.com>
2017-08-13 13:47:18 +0100

    map working in summer

--- src/JaVerT/examples/map.js
+++ src/JaVerT/examples/map.js
@@ -1,11 +1,11 @@
 /**
 	***** VALID AND INVALID KEYS *****
 	
-	@pred validKey(key) : 
+	@pred ValidKey(key) : 
 		isNamedProperty(key) *
 		(! (key == "hasOwnProperty"));
 		
-	@pred invalidKey(key) :
+	@pred InvalidKey(key) :
 		types (key : $$undefined_type),
 		types (key : $$null_type),
 		types (key : $$boolean_type),
@@ -15,28 +15,28 @@
 
 /**	
 	@pred Map(m, contents, mp) :
-		ObjectWithProto(m, mp) *
-		dataField(m, "_contents", contents) *
-		standardObject(contents) *
+		JSObjWithProto(m, mp) *
+		DataProp(m, "_contents", contents) *
+		JSObject(contents) *
 		((contents, "hasOwnProperty") -> None) *
 		((m, "get") -> None) *
 		((m, "put") -> None) *
 		types(mp : $$object_type);
   	
   	@pred MapProto(mp) :
-		standardObject(mp) *
-		dataField(mp, "get", #get_loc) * fun_obj(mapGet, #get_loc, #get_proto, #get_sc) *
-		dataField(mp, "put", #put_loc) * fun_obj(mapPut, #put_loc, #put_proto, #put_sc) *
+		JSObject(mp) *
+		DataProp(mp, "get", #get_loc) * FunctionObject(#get_loc, "mapGet", #get_sc, _) *
+		DataProp(mp, "put", #put_loc) * FunctionObject(#put_loc, "mapPut", #put_sc, _) *
 		((mp, "_contents") -> None);
 */
 
 /**
 	@id isValidKey
 	
-	@pre  ((key == #key) * validKey(#key))
+	@pre  ((key == #key) * ValidKey(#key))
 	@post (ret == $$t)
 		
-	@pre ((key == #key) * invalidKey(#key))
+	@pre ((key == #key) * InvalidKey(#key))
 	@post (ret == $$f)
 */
 function isValidKey(key) {
@@ -48,7 +48,8 @@ function isValidKey(key) {
     @id  map
 
     @pre (
-    	ObjectWithProto(this, #mp) *
+    	initialHeapPost() * 
+    	JSObjWithProto(this, #mp) *
         ((this, "_contents") -> None) *
         ((this, "get") -> None) *
         ((this, "put") -> None) *
@@ -56,6 +57,7 @@ function isValidKey(key) {
     )
     
     @post (
+    	initialHeapPost() * 
     	Map(this, #contents, #hp) * 
     	MapProto(#mp)
     )
@@ -68,41 +70,47 @@ function Map () {
 	@id mapGet
 	
 	@pre (
-		(key == #key) * validKey(#key) * 
-		scope(isValidKey : #iVK) * fun_obj(isValidKey, #iVK, #iVK_proto) * 
+		initialHeapPost() * 
+		(key == #key) * ValidKey(#key) * 
+		scope(isValidKey : #iVK) * FunctionObject(#iVK, "isValidKey", _, _) * 
 		Map(this, #contents, #mp) * MapProto(#mp) *
-		dataField(#contents, #key, #v)
+		DataProp(#contents, #key, #v)
 	)
 	
 	@post (
-		scope(isValidKey : #iVK) * fun_obj(isValidKey, #iVK, #iVK_proto) * 
+		initialHeapPost() * 
+		scope(isValidKey : #iVK) * FunctionObject(#iVK, "isValidKey", _, _) * 
 		Map(this, #contents, #mp) * MapProto(#mp) * 
-		dataField(#contents, #key, #v) *
+		DataProp(#contents, #key, #v) *
 		(ret == #v)
 	)
 	
 	@pre (
-		(key == #key) * validKey(#key) * 
-		scope(isValidKey : #iVK) * fun_obj(isValidKey, #iVK, #iVK_proto) * 
+		initialHeapPost() * 
+		(key == #key) * ValidKey(#key) * 
+		scope(isValidKey : #iVK) * FunctionObject(#iVK, "isValidKey", _, _) * 
 		Map(this, #contents, #mp) * MapProto(#mp) *
-		emptyField(#contents, #key)
+		EmptyProp(#contents, #key)
 	)
 	
 	@post (
-		scope(isValidKey : #iVK) * fun_obj(isValidKey, #iVK, #iVK_proto) * 
+		initialHeapPost() * 
+		scope(isValidKey : #iVK) * FunctionObject(#iVK, "isValidKey", _, _) * 
 		Map(this, #contents, #mp) * MapProto(#mp) * 
-		emptyField(#contents, #key) *
+		EmptyProp(#contents, #key) *
 		(ret == $$null)
 	)
 	
 	@pre (
-		(key == #key) * invalidKey(#key) *
-		scope(isValidKey : #iVK) * fun_obj(isValidKey, #iVK, #iVK_proto)
+		initialHeapPost() * 
+		(key == #key) * InvalidKey(#key) *
+		scope(isValidKey : #iVK) * FunctionObject(#iVK, "isValidKey", _, _)
 	)
 	
 	@posterr (
+		initialHeapPost() * 
 		ErrorObjectWithMessage(err, "Invalid Key") *
-		scope(isValidKey : #iVK) * fun_obj(isValidKey, #iVK, #iVK_proto)
+		scope(isValidKey : #iVK) * FunctionObject(#iVK, "isValidKey", _, _)
 	)
 */
 Map.prototype.get = function getValue (key) {
@@ -118,56 +126,64 @@ Map.prototype.get = function getValue (key) {
 	@id mapPut
 
 	@pre (
-		(key == #key) * validKey(#key) * 
+		initialHeapPost() * 
+		(key == #key) * ValidKey(#key) * 
 		(value == #value) *
-		scope(isValidKey : #iVK) * fun_obj(isValidKey, #iVK, #iVK_proto) * 
+		scope(isValidKey : #iVK) * FunctionObject(#iVK, "isValidKey", _, _) * 
 		Map(this, #contents, #mp) * MapProto(#mp) *
-		emptyField(#contents, #key)
+		EmptyProp(#contents, #key)
 	)
 	
 	@post (
-		scope(isValidKey : #iVK) * fun_obj(isValidKey, #iVK, #iVK_proto) * 
+		initialHeapPost() * 
+		scope(isValidKey : #iVK) * FunctionObject(#iVK, "isValidKey", _, _) * 
 		Map(this, #contents, #mp) * MapProto(#mp) * 
-		dataField(#contents, #key, #value)
+		DataProp(#contents, #key, #value)
 	)
 	
 	@pre (
-		(key == #key) * validKey(#key) * 
+		initialHeapPost() * 
+		(key == #key) * ValidKey(#key) * 
 		(value == #value) *
-		scope(isValidKey : #iVK) * fun_obj(isValidKey, #iVK, #iVK_proto) * 
+		scope(isValidKey : #iVK) * FunctionObject(#iVK, "isValidKey", _, _) * 
 		Map(this, #contents, #mp) * MapProto(#mp) *
-		dataFieldGeneral(#contents, #key, #oldValue, $$t, #enum, #conf) 
+		DataPropGen(#contents, #key, #oldValue, $$t, #enum, #conf) 
 	)
 	
 	@post (
-		scope(isValidKey : #iVK) * fun_obj(isValidKey, #iVK, #iVK_proto) * 
+		initialHeapPost() * 
+		scope(isValidKey : #iVK) * FunctionObject(#iVK, "isValidKey", _, _) * 
 		Map(this, #contents, #mp) * MapProto(#mp) * 
-		dataFieldGeneral(#contents, #key, #value, $$t, #enum, #conf)
+		DataPropGen(#contents, #key, #value, $$t, #enum, #conf)
 	)
 	
 	@pre (
-		(key == #key) * validKey(#key) * 
+		initialHeapPost() * 
+		(key == #key) * ValidKey(#key) * 
 		(value == #value) *
-		scope(isValidKey : #iVK) * fun_obj(isValidKey, #iVK, #iVK_proto) * 
+		scope(isValidKey : #iVK) * FunctionObject(#iVK, "isValidKey", _, _) * 
 		Map(this, #contents, #mp) * MapProto(#mp) *
-		dataFieldGeneral(#contents, #key, #oldValue, $$f, #enum, #conf) 
+		DataPropGen(#contents, #key, #oldValue, $$f, #enum, #conf) 
 	)
 	
 	@posterr (
-		scope(isValidKey : #iVK) * fun_obj(isValidKey, #iVK, #iVK_proto) * 
+		initialHeapPost() * 
+		scope(isValidKey : #iVK) * FunctionObject(#iVK, "isValidKey", _, _) * 
 		Map(this, #contents, #mp) * MapProto(#mp) * 
-		dataFieldGeneral(#contents, #key, #oldValue, $$f, #enum, #conf) *
+		DataPropGen(#contents, #key, #oldValue, $$f, #enum, #conf) *
 		isTypeError(err)
 	)
 	
 	@pre (
-		(key == #key) * invalidKey(#key) *
-		scope(isValidKey : #iVK) * fun_obj(isValidKey, #iVK, #iVK_proto)
+		initialHeapPost() * 
+		(key == #key) * InvalidKey(#key) *
+		scope(isValidKey : #iVK) * FunctionObject(#iVK, "isValidKey", _, _)
 	)
 	
 	@posterr (
+		initialHeapPost() * 
 		ErrorObjectWithMessage(err, "Invalid Key") *
-		scope(isValidKey : #iVK) * fun_obj(isValidKey, #iVK, #iVK_proto)
+		scope(isValidKey : #iVK) * FunctionObject(#iVK, "isValidKey", _, _)
 	)
 */
 Map.prototype.put = function (key, value) {
