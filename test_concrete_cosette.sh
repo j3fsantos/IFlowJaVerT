450f91da5bd73acbfcc8cdab11d70ff62352d02d
José Santos <j3fsantos@gmail.com>
2017-11-20 12:33:14 +0000

    a script for testing concrete code

---
+++ test_concrete_cosette.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+for filename in ./$1/*.js; do
+	time {
+	echo "Next file: $filename"
+	name=$(echo $filename | cut -f 2 -d '.')
+	#echo $name
+	jname='.'$name'.jsil'
+	#echo $jname
+	./js2jsil.native -file "$filename" -cosette &> /dev/null
+	./jsil2rkt.native -file "$jname" -js 
+	rname='.'$name'.rkt'
+	#echo $rname
+	mv "$rname" . 
+	rname=$(basename $rname)
+	#echo $rname
+	res=$(racket $rname | tail -n1)
+	#echo $res
+	if [[ $res == "(unsat)"* ]]; then
+		echo "Pass: $filename" 
+	else
+		echo "Fail: $filename" 
+	fi }
+	echo "----------------" }
+done
