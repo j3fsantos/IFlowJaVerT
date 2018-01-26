ff9ca3f4ea6507edc8125406adbf840b22b24c1a
pmaksimo <p.maksimovic@imperial.ac.uk>
2018-01-26 08:17:06 -0700

    Removing test262 environment

--- setup_environment.sh
+++ setup_environment.sh
@@ -6,5 +6,5 @@ cp src/JS2JSIL/runtime/*.jsil environment
 cp src/JS2JSIL/runtime/harness.js environment
 cp src/examples/JSIL/*.jsil environment
 
-rm -rf test262/environment
-cp -r environment test262/environment
+# rm -rf test262/environment
+# cp -r environment test262/environment
