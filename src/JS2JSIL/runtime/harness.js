6408ca531195d89c176596ef2828ac2509f3992d
pmaksimo <p.maksimovic@imperial.ac.uk>
2018-01-25 17:30:38 -0700

    harness

--- src/JS2JSIL/runtime/harness.js
+++ src/JS2JSIL/runtime/harness.js
@@ -291,7 +291,6 @@ var msPerSecond = 1000;
 var msPerMinute = 60000;
 var msPerHour = 3600000;
 
-/*
 var date_1899_end = -2208988800001;
 var date_1900_start = -2208988800000;
 var date_1969_end = -1;
@@ -301,4 +300,3 @@ var date_2000_start = 946684800000;
 var date_2099_end = 4102444799999;
 
 var date_2100_start = 4102444800000;
-*/
