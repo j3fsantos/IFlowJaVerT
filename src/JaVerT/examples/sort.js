c446e5eae24e39aeabf8983445412d9e5fe8c748
pmaksimo <p.maksimovic@imperial.ac.uk>
2018-03-10 13:23:38 +0000

    fff

--- src/JaVerT/examples/sort.js
+++ src/JaVerT/examples/sort.js
@@ -1,28 +1,52 @@
 /**
 
+
+  ###
+  ### Nullable objects are either objects or null
+  ###
   @pred nullableObject(o) : 
-    types(o : Obj),
-    (o == null);
+    [nOb01] types(o : Obj),
+    [nOb02] (o == null);
 
- @pred Node(+n:Obj, v:Num, t):
-   JSObject(n) *
-   DataProp(n, "value", v) *
-   DataProp(n, "next", t);
- 
- @pred NDList(+l, E:Set):
-   (l == null) * (E == -{ }-),
+  ###
+  ### A node is an object with a property 'value' (number)
+  ### and a pointer to the next node 'next' (nullableObject)
+  ###
+  ### In-parameters: n
+  ###
+  @pred Node(+n:Obj, v:Num, t):
+    JSObject(n) *
+    DataProp(n, "value", v) *
+    DataProp(n, "next", t);
+  
+  ###
+  ### A nodelist is a singly-linked list of nodes 
+  ### starting from nl (nullableObject) and
+  ### carrying the SET of values of that list (E)
+  ###
+  ### In-parameters: nl
+  ###
+  @pred NDList(+nl, E:Set):
+    (l == null) * (E == -{ }-),
 
-   Node(l, #v, #t) * NDList(#t, #tE) *
-   (E == -u- (#tE, -{ #v }-)) *
-   (!(#v --e-- #tE));
+    Node(nl, #v, #t) * NDList(#t, #tE) *
+    (E == -u- (#tE, -{ #v }-)) *
+    (! (#v --e-- #tE));
 
- @pred SOList(+l, E:Set):
-   (l == null) * (E == -{ }-),
+  ###
+  ### A sorted list is a singly-linked list of nodes 
+  ### starting from nl (nullableObject),
+  ### with values sorted in ascending order,
+  ### carrying the SET of values of that list (E),
+  ###
+  ### In-parameters: nl
+  ###
+  @pred SOList(+nl, E:Set):
+    (nl == null) * (E == -{ }-),
 
-   Node(l, #v, #t) * SOList(#t, #tE) *
-   (E == -u- (#tE, -{ #v }-)) *
-   (forall #x: Num. ((! (#x --e-- #tE)) \/ (#v <# #x)));
-   
+    Node(nl, #v, #t) * SOList(#t, #tE) *
+    (E == -u- (#tE, -{ #v }-)) *
+    (forall #x:Num. ((! (#x --e-- #tE)) \/ (#v <# #x)));
  */
  
 /**
@@ -39,7 +63,6 @@ function insert(node, value) {
 
     /** @tactic unfold SOList(#n, #E) */
     if (node === null) {
-    	/** @tactic fold SOList(#n, #E) */
         result = { next: null, value: value }
     } else if (node.value === value) {
         result = node;
