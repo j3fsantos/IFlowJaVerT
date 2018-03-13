5cdfb68562c9cc3c77245cf0a5dcb5544c5f548b
Théotime Grohens <tgrohens@gmail.com>
2018-03-13 17:00:27 +0000

    remove extra prints

--- test_single_file.sh
+++ test_single_file.sh
@@ -2,19 +2,19 @@
 
 # copy file to current folder
 cp $1 .
-echo "filename:" $1
+#echo "filename:" $1
 filename=$(basename $1)
-echo "basename:" $filename
+#echo "basename:" $filename
 name=${filename%%.*}
-echo $name
+#echo $name
 jname=$name'.jsil'
-echo $jname
+#echo $jname
 rname=$name'.rkt'
-echo $rname
+#echo $rname
 ./js2jsil.native -file "$filename" -cosette &> /dev/null
 ./jsil2rkt.native -file "$jname" -js 
 res=$(racket $rname | tail -n1)
-#echo $res
+echo $res
 if [[ $res == "(unsat)"* ]]; then
 	echo "Pass: $filename" 
 else
