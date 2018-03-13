6b14d52150d9f5b029d78f04cde122b444b7e643
Théotime Grohens <tgrohens@gmail.com>
2018-03-13 16:13:27 +0000

    new script for testing a single file

---
+++ test_single_file.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# copy file to current folder
+cp $1 .
+echo "filename:" $1
+filename=$(basename $1)
+echo "basename:" $filename
+name=${filename%%.*}
+echo $name
+jname=$name'.jsil'
+echo $jname
+rname=$name'.rkt'
+echo $rname
+./js2jsil.native -file "$filename" -cosette &> /dev/null
+./jsil2rkt.native -file "$jname" -js 
+res=$(racket $rname | tail -n1)
+#echo $res
+if [[ $res == "(unsat)"* ]]; then
+	echo "Pass: $filename" 
+else
+	echo "Fail: $filename" 
+fi
