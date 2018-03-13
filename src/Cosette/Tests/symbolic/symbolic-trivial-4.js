472df55ec6188d17f7f311ed864653dc9bbcc247
José Santos <j3fsantos@gmail.com>
2018-03-13 16:14:46 +0000

    fixing the syntax in trivial symbolic examples - up to 4 so far

--- src/Cosette/Tests/symbolic/symbolic-trivial-4.js
+++ src/Cosette/Tests/symbolic/symbolic-trivial-4.js
@@ -1,7 +1,7 @@
-var n = jsil_make_symbolic_number (); 
-jsil_assume ((n > 0) && (n < 2000));
+var n = symb_number (n); 
+Assume ((0 < n) and (n < 2000));
 var o = {}; 
 o.field = n;  
 var y = 3 + 7 + o.field;
 var z = y + 7;
-jsil_Assert(z < 0); 
+Assert(not(z < 0)); 
