fbd8879b9855537c72f8c678c14e9c3595b52df5
pmaksimo <p.maksimovic@imperial.ac.uk>
2018-03-16 17:44:52 +0000

    plus, times fixed

--- src/Cosette/tests/test262/plus.js
+++ src/Cosette/tests/test262/plus.js
@@ -1,22 +1,23 @@
 // Comes from: ch11/11.6/11.6.1/S11.6.1_A2.1_T1.js
 
 //CHECK#1
-Assert(1 + 1 = 2);
+var x = 1 + 1;
+Assert(x = 2);
 
 //CHECK#2
-var x = symb_number(n1);
-var x1 = x + 1;
+var n1 = symb_number(n1);
+var x1 = n1 + 1;
 Assert(x1 = n1 + 1);
 
 //CHECK#3
-var y = symb_number(n2);
-var y1 = 1 + y;
+var n2 = symb_number(n2);
+var y1 = 1 + n2;
 Assert(y1 = 1 + n2);
 
 //CHECK#4
-var x = symb_number(n3);
-var y = symb_number(n4);
-var xy = x + y;
+var n3 = symb_number(n3);
+var n4 = symb_number(n4);
+var xy = n3 + n4;
 Assert(xy = n3 + n4);
 
 //CHECK#5
@@ -24,8 +25,11 @@ var o1 = new Object();
 var o2 = new Object();
 var s = symb_string(s1);
 
-o1[s] = symb_number(n5);
-o2[s] = symb_number(n6);
+var n5 = symb_number(n5);
+var n6 = symb_number(n6);
+
+o1[s] = n5;
+o2[s] = n6;
 var x56 = o1[s] + o2[s];
 
 Assert(x56 = n5 + n6);
--- src/Cosette/tests/test262/times.js
+++ src/Cosette/tests/test262/times.js
@@ -1,9 +1,9 @@
 // Comes from: ch11/11.5/11.5.1/S11.5.1_A4_T2.js
 
 //CHECK#1
-var x = symb_number (n1);
-var y = symb_number (n2);
-var xy = x * y; // JS-level multiplication
+var n1 = symb_number (n1);
+var n2 = symb_number (n2);
+var xy = n1 * n2;     // JS-level multiplication
 Assert(xy = n1 * n2); // Symbolic-level multiplication
 
 // what to do with these ?
@@ -46,8 +46,8 @@ Assert(xy = n1 * n2); // Symbolic-level multiplication
 //
 
 // Division tests, abstracting ch11/11.5/11.5.2/S11.5.2_A4_T2.js
-var x = symb_number (n1);
-var y = symb_number (n2);
-var xy = x / y; // JS-level multiplication
-Assert(xy = n1 / n2); // Symbolic-level multiplication
+var n3 = symb_number (n3);
+var n4 = symb_number (n4);
+var xy = n3 / n4; // JS-level multiplication
+Assert(xy = n3 / n4); // Symbolic-level multiplication
 
