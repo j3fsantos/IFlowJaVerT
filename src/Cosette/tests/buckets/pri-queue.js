4578f200f02a89c8e592df00ef0c232e2f98d3b2
pmaksimo <p.maksimovic@imperial.ac.uk>
2018-03-19 14:46:26 +0000

    current

--- src/Cosette/tests/buckets/pri-queue.js
+++ src/Cosette/tests/buckets/pri-queue.js
@@ -347,21 +347,16 @@ var x2 = symb_number(x2); // 1
 // var x2 = 1;
 // var x3 = 3;
 
-Assume (x2 < x1); // x1 = 2, x2 = 3, x3 = 1
-
 queue.enqueue(x1);
 queue.enqueue(x2);
 // queue.enqueue(x3);
 
-var l0 = queue.size();
 var y1 = queue.dequeue(); // y1 = 3 = x2
-var l1 = queue.size();
 var y2 = queue.dequeue(); // y2 = 2 = x1
-var l2 = queue.size();
 // var y3 = queue.dequeue(); // y3 = 1 = x3
 
 // console.log(y1);
 // console.log(y2);
 // console.log(y3);
 
-Assert((l0 = 2) and (l1 = 1) and (l2 = 0) and (y1 = x2) and (y2 = x1));
+Assert (((x2 < x1) and (y1 = x1) and (y2 = x2)) or ((x1 = x2) and (y1 = x1) and (y2 = x2)) or ((x1 < x2) and (y1 = x2) and (y2 = x1)));
