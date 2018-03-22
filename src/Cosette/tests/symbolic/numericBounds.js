e1de2a36dadbe39e7f34577e901fb1b8ef7a392b
José Santos <j3fsantos@gmail.com>
2018-03-22 13:38:00 +0000

    more examples

---
+++ src/Cosette/tests/symbolic/numericBounds.js
@@ -0,0 +1,13 @@
+var i = symb_number (i); 
+var output = 0; 
+
+Assume (i < 3); 
+
+while (i > 0) { 
+	output += i; 
+ 	i--; 
+}
+
+
+Assert (output > 20);
+
---
+++ src/Cosette/tests/symbolic/primes.js
@@ -0,0 +1,22 @@
+function isPrime(value) {
+    var primes = [];
+    for(var i = 2; i <= value; i++) {
+        primes[i] = true;
+    }
+    var limit = value;
+    for(var i = 2; i <= limit; i++) {
+        if(primes[i] === true) {
+            for(var j = i * i; j <= value; j += i) {
+                primes[j] = false;
+            }
+        }
+    }
+    return primes[value];
+}
+
+var n = symb_number(n);
+Assume(n >= 2);
+Assume(n <= 10);
+
+var res = isPrime(n);
+Assert(res = $$t);
