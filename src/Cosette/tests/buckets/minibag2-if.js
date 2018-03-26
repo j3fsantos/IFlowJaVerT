b2151ecca31a4e801dfb7f3c8d33689ab57996dd
Théotime Grohens <tgrohens@gmail.com>
2018-03-26 16:44:04 +0100

    test files

--- src/Cosette/tests/buckets/minibag2-if.js
+++ src/Cosette/tests/buckets/minibag2-if.js
@@ -311,18 +311,20 @@ buckets.Bag = function (toStrFunction) {
 var s1 = symb_string(s1);
 var s2 = symb_string(s2);
 var s3 = symb_string(s3);
-Assume ((not (s1 = s2)) and (not (s1 = s3)));
+//Assume ((not (s1 = s2)) and (not (s1 = s3)));
 // Assume (not (s-nth (s1, 0) = "@"));
 // Assume (not (s-nth (s2, 0) = "@"));
 // Assume (not (s-nth (s3, 0) = "@"));
 
 var bag = new buckets.Bag();
 
-bag.add(s1);
-bag.add(s2);
-bag.add(s3);
-bag.remove(s2);
-bag.remove(s2); // if s2 = s3, we remove both, otherwise s3 is still in there
-var res = bag.size();
-//Assert (((s2 = s3) and (res = 1)) or ((not (s2 = s3)) and (res = 2)));
-Assert(((s1 = s2) and (res = 1)) or ((not (s1 = s2)) and (s2 = s3) and (res = 1)) or ((not (s1 = s2)) and (not (s2 = s3)) and (res = 2)))
+if ((s1 != s2) && (s1 != s3)) {
+  bag.add(s1);
+  bag.add(s2);
+  bag.add(s3);
+  bag.remove(s2);
+  bag.remove(s2); // if s2 = s3, we remove both, otherwise s3 is still in there
+  var res = bag.size();
+  Assert (((s2 = s3) and (res = 1)) or ((not (s2 = s3)) and (res = 2)));
+}
+//Assert(((s1 = s2) and (res = 1)) or ((not (s1 = s2)) and (s2 = s3) and (res = 1)) or ((not (s1 = s2)) and (not (s2 = s3)) and (res = 2)))
