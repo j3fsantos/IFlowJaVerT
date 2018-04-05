f6f54c49a17dc0734b45752bf6263c09690bf3d4
pmaksimo <p.maksimovic@imperial.ac.uk>
2018-04-05 15:41:49 +0100

    sfin

--- src/Cosette/tests/symbolic/simple-forin.js
+++ src/Cosette/tests/symbolic/simple-forin.js
@@ -2,6 +2,8 @@
 // this one is interesting (use hasOwnProperty)
 // make one fail (outcome = 60) and 
 
+var p1_string = symb_string(p1_string);
+Assume(not (s-nth(p1_string, 0) = "@"));
 
 function top(o) {
   var count = 0;
@@ -13,12 +15,5 @@ function top(o) {
 
 var o = { a: 1, b: 2, c: 3 };
 
-var p1_string = symb_string(p1_string);
-
 o[ p1_string ] = 4;
-
 var res = top(o);
-
-// var truc = symb_number(truc);
-// Assume(truc = res);
-// Assert(truc = 60);
