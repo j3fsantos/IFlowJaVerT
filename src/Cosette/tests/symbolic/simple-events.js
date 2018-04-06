24958b067b3c7306c9f5176b94aadc46ee96c2d6
Théotime Grohens <tgrohens@gmail.com>
2018-04-06 14:41:38 +0100

    simple-events.js

--- src/Cosette/tests/symbolic/simple-events.js
+++ src/Cosette/tests/symbolic/simple-events.js
@@ -1,28 +1,5 @@
 var o = {
-  // fib: function f_fib(x) {
-  //   if (x < 3) {
-  //     return 1;
-  //   } else {
-  //     return this.fib(x-1) + this.fib(x-2);
-  //   }
-  // },
-  
-  // fact: function f_fact(x) {
-  //   if (x < 1) {
-  //     return 1;
-  //   } else {
-  //     return this.fact(x-1) * x;
-  //   }
-  // },
-  
-  // sum: function f_sum(x) {
-  //   if (x < 1) {
-  //     return 0;
-  //   } else {
-  //     return this.sum(x-1) + x;
-  //   }
-  // },
-  
+
   plusOne: function(x) {
     return x + 1;
   },
@@ -30,18 +7,20 @@ var o = {
   minusOne: function(x) {
      return x - 1;
    },
-  // 
-  // timesTwo: function(x) {
-  //   return x * 2;
-  // },
+
 };
 
 var s1 = symb_string(s1);
-// Assume((s1 = "fib") or (s1 = "fact") or (s1 = "sum"));
-//Assume((s1 = "plusOne"));
+var s2 = symb_string(s2);
+//Assume((s1 = "plusOne") or (s1 = "minusOne"));
+//Assume((s2 = "plusOne") or (s2 = "minusOne"));
+
 var n1 = symb_number(n1);
+var n2 = symb_number(n2);
+Assume(not (n1 = n2));
 
-// figure out which function to call
-var total = o[s1](n1);
+// figure out which functions to call
+var total1 = o[s1](n1);
+var total2 = o[s2](n2);
 
-Assert(total = 10);
+Assert(total1 = total2);
