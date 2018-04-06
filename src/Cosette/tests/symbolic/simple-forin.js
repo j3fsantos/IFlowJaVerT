01f142cc3e376c263f934fff36698c6019c1b4c1
pmaksimo <p.maksimovic@imperial.ac.uk>
2018-04-06 09:06:31 +0100

    8

--- src/Cosette/tests/symbolic/simple-forin.js
+++ src/Cosette/tests/symbolic/simple-forin.js
@@ -11,9 +11,9 @@ function top(o) {
   return count;
 }
 
-var o = { a: 1, b: 2, c: 3 };
+var o = { a: 1, b: 2, c: 3, d: 4, e: 5, f: 6, g: 7 };
 
 o[ p1_string ] = 4;
 var res = top(o);
 
-Assert(res = 4);
+Assert(res = 8);
