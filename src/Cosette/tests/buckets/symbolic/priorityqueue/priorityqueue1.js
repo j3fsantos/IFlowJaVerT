d813d3965398de63f945926ce91e6dacb8611479
Théotime Grohens <tgrohens@gmail.com>
2018-04-14 12:58:19 +0100

    priorityqueue1: enqueue, add, dequeue, peek

--- src/Cosette/tests/buckets/symbolic/priorityqueue/priorityqueue1.js
+++ src/Cosette/tests/buckets/symbolic/priorityqueue/priorityqueue1.js
@@ -720,12 +720,15 @@ var x2 = symb_number(x2);
 var x3 = symb_number(x3);
 
 pqueue.enqueue(x1);
-pqueue.enqueue(x2);
+pqueue.add(x2);
 pqueue.enqueue(x3);
 
 
-var y1 = pqueue.dequeue();
+var y1 = pqueue.peek();
+pqueue.dequeue();
 var y2 = pqueue.dequeue();
 var y3 = pqueue.dequeue();
-
 Assert((y1 >= y2) and (y2 >= y3));
+
+var y4 = pqueue.dequeue();
+Assert(y4 = undefined);
