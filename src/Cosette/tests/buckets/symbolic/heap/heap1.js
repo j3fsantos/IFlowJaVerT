f0ce66755cb5db83691be30ce3add374e818f554
Théotime Grohens <tgrohens@gmail.com>
2018-04-14 16:35:27 +0100

    heap1 simplified: siftUp*, peek, add

--- src/Cosette/tests/buckets/symbolic/heap/heap1.js
+++ src/Cosette/tests/buckets/symbolic/heap/heap1.js
@@ -573,11 +573,12 @@ Assume(not (x3 = x4));
 
 var res2 = heap.add(undefined);
 Assert(res2 = undefined);
+var res3 = heap.peek();
+Assert(res3 = undefined);
+
 heap.add(x1);
 heap.add(x2);
 heap.add(x3);
 heap.add(x4);
 
-var res = heap.removeRoot();
-Assert(((x1 < x2) and (x1 < x3) and (x1 < x4) and (res = x1)) or ((x2 < x1) and (x2 < x3) and (x2 < x4) and (res = x2)) or ((x3 < x1) and (x3 < x2) and (x3 < x4) and (res = x3)) or ((x4 < x1) and (x4 < x2) and (x4 < x1) and (res = x4)));
-
+var res1 = heap.peek();
