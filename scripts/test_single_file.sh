3c8c01943b783b1d534eb7020f53dd30498b5a91
Théotime Grohens <tgrohens@gmail.com>
2018-04-14 11:45:11 +0100

    scripts

--- scripts/test_single_file.sh
+++ scripts/test_single_file.sh
@@ -12,7 +12,7 @@ logname=res_$name.txt
 ./js2jsil.native -file "$filename" -cosette -line_numbers
 ./jsil2rkt.native -file "$jsilname" -js 
 time racket $rktname > $logname
-tail -n 1 $logname
+tail -n 12 $logname
 
 mv models.json $name"_models.json"
 mv coverage.txt $name"_raw_coverage.txt"
