ddd6e97e9d1d5ba79758e996d9f1cd9f3b5c6f3f
Théotime Grohens <tgrohens@gmail.com>
2018-04-15 18:03:20 +0100

    multidictionary2: remove, containsKey

--- src/Cosette/tests/buckets/symbolic/multidictionary/multidictionary2.js
+++ src/Cosette/tests/buckets/symbolic/multidictionary/multidictionary2.js
@@ -762,3 +762,15 @@ var res1 = dict.remove(s1, x1);
 Assert(res1);
 var res2 = dict.remove(s1, x2);
 Assert(((s1 = s2) and (not (x1 = x2)) and res2) or (((not (s1 = s2)) or ((s1 = s2) and (x1 = x2))) and (not res2)));
+
+var dict2 = new buckets.MultiDictionary();
+dict2.set(s1, x1);
+dict2.set(s1, x2);
+var res3 = dict2.remove(s1);
+Assert(res3);
+var res5 = dict2.remove(s1);
+Assert(not res5);
+
+dict2.set(s2, x1);
+var res4 = dict2.remove(s2, x2);
+Assert(not res4);
