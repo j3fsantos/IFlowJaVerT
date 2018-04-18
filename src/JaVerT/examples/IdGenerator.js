f05fb91d45b840c9577ab3406705b5fe262ce633
pmaksimo <p.maksimovic@imperial.ac.uk>
2018-04-18 14:34:48 +0100

    JaVerT tests

--- src/JaVerT/examples/IdGenerator.js
+++ src/JaVerT/examples/IdGenerator.js
@@ -1,10 +1,9 @@
 /**
-@pred idGenerator(ig, sc_ig, c_val, prefix) :
+@pred idGenerator(+ig, +sc_ig, c_val : Num, prefix : Str) :
    JSObject(ig) * 
-   DataProp(ig, "getId", #gni) * FunctionObject(#gni, "getId", #gni_sc, _) *
-   DataProp(ig, "reset", #ri)  * FunctionObject(#ri, "reset", #ri_sc, _) *
-   closure(count: c_val, prefix: prefix; getId: #gni_sc, reset: #ri_sc, makeIdGen: sc_ig) * 
-   types (c_val: $$number_type, prefix: $$string_type);
+   DataProp(ig, "getId", #gni) * JSFunctionObject(#gni, "getId", #gni_sc, _, _) *
+   DataProp(ig, "reset", #ri)  * JSFunctionObject(#ri, "reset", #ri_sc, _, _) *
+   closure(count: c_val, prefix: prefix; getId: #gni_sc, reset: #ri_sc, makeIdGen: sc_ig);
 */
 
 /**
@@ -13,7 +12,7 @@
 	   scope(makeIdGen: #mig) *
 	   scope(ig1: #ig1) *
 	   scope(ig2: #ig2) *
-	   FunctionObject(#mig, "makeIdGen", _, _) *
+	   JSFunctionObject(#mig, "makeIdGen", _, _, _) *
 	   idGenerator(#ig1, _, 1, "foo") *
 	   idGenerator(#ig2, _, 0, "bar") * 
 	   scope(id1: #id1) *
@@ -22,7 +21,7 @@
 
 /**
 	@id   makeIdGen
-	@pre  ((prefix == #prefix) * types(#prefix: $$string_type))
+	@pre  ((prefix == #prefix) * types(#prefix: Str))
 	@post (idGenerator(ret, $$scope, 0, #prefix))
 */
 var makeIdGen = function (prefix) { 
@@ -32,7 +31,7 @@ var makeIdGen = function (prefix) {
 	/**
 		@id   getId
 		@pre  (this == #ig) * o_chains(getId: $$scope, makeIdGen: #sc_ig) * idGenerator(#ig, #sc_ig, #c, #prefix)
-		@post idGenerator(#ig, #sc_ig, #c + 1, #prefix) * (ret == (#prefix ++ "_id_" ++ num_to_string(#c)))
+		@post idGenerator(#ig, #sc_ig, #c + 1, #prefix) * (ret == #prefix ++ "_id_" ++ num_to_string(#c))
 		
 	*/
 	var getId = function () { 
