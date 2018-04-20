16f5815a2e6199dde61e87d3f438c89fa6ad4bca
Théotime Grohens <tgrohens@gmail.com>
2018-04-20 11:32:36 +0100

    overall coverage: aggregated results

--- scripts/overall_coverage.py
+++ scripts/overall_coverage.py
@@ -61,12 +61,17 @@ def make_overall_coverage(cov):
 
     return overall
     
-def print_overall_coverage(overall_cov):
+def print_overall_coverage(overall_cov, test_name):
     
     # coloring stuff
     green = colored.attr('bold') + colored.fg('green')
     red = colored.attr('bold') + colored.fg('red')
+    blue = colored.attr('bold') + colored.fg('blue')
     reset = colored.attr('reset')
+
+    # get all functions associated with the test
+    total_executable_lines = 0
+    total_executed_lines = 0
     
     for fname in overall_cov:
         executable_lines = set(overall_cov[fname]['executable'])
@@ -74,6 +79,12 @@ def print_overall_coverage(overall_cov):
         missing_lines = list(executable_lines.difference(executed_lines))
         missing_lines = sorted(missing_lines)
         
+        # aggregated info
+        if fname.startswith(test_name):
+            print(fname)
+            total_executable_lines += len(executable_lines)
+            total_executed_lines += len(executed_lines)
+
         if missing_lines == []:
             print(green + '{}: executed all lines'.format(fname))
         else:
@@ -84,11 +95,25 @@ def print_overall_coverage(overall_cov):
         coverage_prop = (nb_exec_lines - nb_missing_lines)/nb_exec_lines
         print('{}: coverage {:.2%}\n'.format(fname, coverage_prop) + reset)
 
+    # final print
+    total_prop = total_executed_lines / total_executable_lines
+    print(blue + '{}: {}/{} ({:.2%}) lines executed'.format(test_name,
+                                                            total_executed_lines,
+                                                            total_executable_lines,
+                                                            total_prop) + reset)
+
+
+def get_test_name(filenames):
+    base_name = filenames[0].split('.')[0]
+    test_name = base_name.split('-')[0]
+    return test_name
+
 if __name__ == "__main__":
     if len(sys.argv) < 2:
         print('No input files provided. Aborting')
     else:
         filenames = sys.argv[1:]
+        test_name = get_test_name(filenames)
         coverage = load_coverage(filenames)
         overall_coverage = make_overall_coverage(coverage)
-        print_overall_coverage(overall_coverage)
+        print_overall_coverage(overall_coverage, test_name)
